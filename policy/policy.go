8006223268969bb9d3aa123031590fc1b65c8cc9
Dominik Richter <dominik.richter@gmail.com>
2022-09-21 11:02:36 -0700

    ⭐ hub proto

--- policy/policy.go
+++ policy/policy.go
@@ -1,3 +1,3 @@
 package policy
 
-//go:generate protoc --proto_path=../:. --go_out=. --go_opt=paths=source_relative --rangerrpc_out=. --iam-actions_out=. --rangerrpc-swagger_out=. policy.proto
+//go:generate protoc --proto_path=../:. --go_out=. --go_opt=paths=source_relative --rangerrpc_out=. policy.proto
