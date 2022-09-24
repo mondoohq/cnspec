4442d9758f7c0dc30d9786da0242c5662a431471
Dominik Richter <dominik.richter@gmail.com>
2022-09-23 18:07:45 -0700

    🔨 fix back missing services defintions

--- policy/services.go
+++ policy/services.go
@@ -9,3 +9,27 @@ type Library interface {
 	// PolicyExists checks if the given MRN exists
 	PolicyExists(ctx context.Context, mrn string) (bool, error)
 }
+
+type Services struct {
+	PolicyHub
+	PolicyResolver
+}
+
+// LocalServices is a bundle of all the services for handling policies.
+// It has an optional upstream-handler embedded. If a local service does not
+// yield results for a request, and the upstream handler is defined, it will
+// be used instead.
+type LocalServices struct {
+	DataLake  DataLake
+	Upstream  *Services
+	Incognito bool
+}
+
+// NewLocalServices initializes a reasonably configured local services struct
+func NewLocalServices(datalake DataLake, uuid string) *LocalServices {
+	return &LocalServices{
+		DataLake:  datalake,
+		Upstream:  nil,
+		Incognito: false,
+	}
+}
