23dda0462912fa3304d5da00eb8bd5da5c9ac7a9
Dominik Richter <dominik.richter@gmail.com>
2022-09-25 14:56:57 -0700

    ✨ migrate set query to inmemory

--- internal/datalakes/inmemory/policyhub.go
+++ internal/datalakes/inmemory/policyhub.go
@@ -40,6 +40,17 @@ func (db *Db) GetQuery(ctx context.Context, mrn string) (*policy.Mquery, error) 
 	return (q.(wrapQuery)).Mquery, nil
 }
 
+// SetQuery stores a given query
+// Note: the query must be defined, it cannot be nil
+func (db *Db) SetQuery(ctx context.Context, mrn string, mquery *policy.Mquery, isScored bool) error {
+	v := wrapQuery{mquery, isScored}
+	ok := db.cache.Set(dbIDQuery+mrn, v, 1)
+	if !ok {
+		return errors.New("failed to save query '" + mrn + "' to cache")
+	}
+	return nil
+}
+
 // GetRawPolicy retrieves the policy without fixing any invalidations (fast)
 func (db *Db) GetRawPolicy(ctx context.Context, mrn string) (*policy.Policy, error) {
 	q, ok := db.cache.Get(dbIDPolicy + mrn)
@@ -64,6 +75,139 @@ func (db *Db) GetPolicyFilters(ctx context.Context, mrn string) ([]*policy.Mquer
 	}
 
 	return res, nil
+}
+
+// SetPolicy stores a given policy in the data lake
+func (db *Db) SetPolicy(ctx context.Context, policyObj *policy.Policy, filters []*policy.Mquery) error {
+	_, err := db.setPolicy(ctx, policyObj, filters)
+	return err
+}
+
+func (db *Db) setPolicy(ctx context.Context, policyObj *policy.Policy, filters []*policy.Mquery) (wrapPolicy, error) {
+	var err error
+
+	// we may use the cached parents if this policy already exists i.e. if it's
+	// alrady referenced by others
+	parents := map[string]struct{}{}
+
+	x, exists := db.cache.Get(dbIDPolicy + policyObj.Mrn)
+	if exists {
+		existing := x.(wrapPolicy)
+
+		parents = existing.parents
+
+		if existing.LocalContentChecksum == policyObj.LocalContentChecksum &&
+			existing.LocalExecutionChecksum == policyObj.LocalExecutionChecksum {
+			if existing.GraphContentChecksum != policyObj.GraphContentChecksum ||
+				existing.GraphExecutionChecksum != policyObj.GraphExecutionChecksum {
+				return wrapPolicy{}, db.checkAndInvalidatePolicyBundle(ctx, &existing)
+			}
+			return wrapPolicy{}, nil
+		}
+
+		// fall through, re-create the policy
+	}
+
+	policyObj.AssetFilters = map[string]*policy.Mquery{}
+	for i := range filters {
+		filter := filters[i]
+		policyObj.AssetFilters[filter.Mrn] = filter
+		if err = db.SetQuery(ctx, filter.Mrn, filter, false); err != nil {
+			return wrapPolicy{}, err
+		}
+	}
+
+	children := policyObj.DependentPolicyMrns()
+	for childMrn := range children {
+		y, ok := db.cache.Get(dbIDPolicy + childMrn)
+		if !ok {
+			return wrapPolicy{}, errors.New("failed to get child policy '" + childMrn + "'")
+		}
+		child := y.(wrapPolicy)
+
+		child.parents[policyObj.Mrn] = struct{}{}
+		ok = db.cache.Set(dbIDPolicy+childMrn, child, 2)
+		if !ok {
+			return wrapPolicy{}, errors.New("failed to save child policy '" + childMrn + "' to cache")
+		}
+	}
+
+	obj := wrapPolicy{
+		Policy:      policyObj,
+		invalidated: false,
+		parents:     parents,
+		children:    children,
+	}
+
+	ok := db.cache.Set(dbIDPolicy+policyObj.Mrn, obj, 2)
+	if !ok {
+		return wrapPolicy{}, errors.New("failed to save policy '" + policyObj.Mrn + "' to cache")
+	}
+
+	list, err := db.listPolicies()
+	if err != nil {
+		return wrapPolicy{}, err
+	}
+
+	list[policyObj.Mrn] = struct{}{}
+	ok = db.cache.Set(dbIDListPolicies, list, 0)
+	if !ok {
+		return wrapPolicy{}, errors.New("failed to update policies list cache")
+	}
+
+	return obj, db.checkAndInvalidatePolicyBundle(ctx, &obj)
+}
+
+func (db *Db) checkAndInvalidatePolicyBundle(ctx context.Context, wrap *wrapPolicy) error {
+	x, ok := db.cache.Get(dbIDBundle + wrap.Policy.Mrn)
+	if !ok {
+		return db.invalidatePolicyAndBundleAncestors(ctx, wrap)
+	}
+
+	bundleObj := x.(wrapBundle)
+	if bundleObj.graphContentChecksum == wrap.Policy.GraphContentChecksum {
+		log.Trace().Str("policy", wrap.Policy.Mrn).Msg("marketplace> policy cache is up-to-date")
+		return nil
+	}
+
+	return db.invalidatePolicyAndBundleAncestors(ctx, wrap)
+}
+
+func (db *Db) invalidatePolicyAndBundleAncestors(ctx context.Context, wrap *wrapPolicy) error {
+	mrn := wrap.Policy.Mrn
+	log.Debug().Str("policy", mrn).Msg("invalidate policy cache")
+
+	// invalidate the policy if its not invalided
+	if wrap.invalidated == false {
+		wrap.invalidated = true
+		db.cache.Set(dbIDPolicy+mrn, *wrap, 2)
+	}
+
+	x, ok := db.cache.Get(dbIDBundle + mrn)
+	if ok {
+		wrapB := x.(wrapBundle)
+
+		// invalidate the policy bundle if its not invalided
+		if wrapB.invalidated == false {
+			wrapB.invalidated = true
+			db.cache.Set(dbIDBundle+mrn, wrapB, 3)
+		}
+	}
+
+	// update all dependencies
+	for parentMrn := range wrap.parents {
+		x, ok := db.cache.Get(dbIDPolicy + parentMrn)
+		if !ok {
+			return errors.New("policy '" + mrn + "' not found")
+		}
+		parent := x.(wrapPolicy)
+
+		if err := db.invalidatePolicyAndBundleAncestors(ctx, &parent); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // DeletePolicy removes a given policy
@@ -202,138 +346,5 @@ func (db *Db) fixInvalidatedPolicy(ctx context.Context, wrap *wrapPolicy) error 
 	if !ok {
 		return errors.New("failed to save policy '" + wrap.Policy.Mrn + "' to cache")
 	}
-	return nil
-}
-
-// SetPolicy stores a given policy in the data lake
-func (db *Db) SetPolicy(ctx context.Context, policyObj *policy.Policy, filters []*policy.Mquery) error {
-	_, err := db.setPolicy(ctx, policyObj, filters)
-	return err
-}
-
-func (db *Db) setPolicy(ctx context.Context, policyObj *policy.Policy, filters []*policy.Mquery) (wrapPolicy, error) {
-	var err error
-
-	// we may use the cached parents if this policy already exists i.e. if it's
-	// alrady referenced by others
-	parents := map[string]struct{}{}
-
-	x, exists := db.cache.Get(dbIDPolicy + policyObj.Mrn)
-	if exists {
-		existing := x.(wrapPolicy)
-
-		parents = existing.parents
-
-		if existing.LocalContentChecksum == policyObj.LocalContentChecksum &&
-			existing.LocalExecutionChecksum == policyObj.LocalExecutionChecksum {
-			if existing.GraphContentChecksum != policyObj.GraphContentChecksum ||
-				existing.GraphExecutionChecksum != policyObj.GraphExecutionChecksum {
-				return wrapPolicy{}, db.checkAndInvalidatePolicyBundle(ctx, &existing)
-			}
-			return wrapPolicy{}, nil
-		}
-
-		// fall through, re-create the policy
-	}
-
-	policyObj.AssetFilters = map[string]*policy.Mquery{}
-	for i := range filters {
-		filter := filters[i]
-		policyObj.AssetFilters[filter.Mrn] = filter
-		if err = db.SetQuery(ctx, filter.Mrn, filter, false); err != nil {
-			return wrapPolicy{}, err
-		}
-	}
-
-	children := policyObj.DependentPolicyMrns()
-	for childMrn := range children {
-		y, ok := db.cache.Get(dbIDPolicy + childMrn)
-		if !ok {
-			return wrapPolicy{}, errors.New("failed to get child policy '" + childMrn + "'")
-		}
-		child := y.(wrapPolicy)
-
-		child.parents[policyObj.Mrn] = struct{}{}
-		ok = db.cache.Set(dbIDPolicy+childMrn, child, 2)
-		if !ok {
-			return wrapPolicy{}, errors.New("failed to save child policy '" + childMrn + "' to cache")
-		}
-	}
-
-	obj := wrapPolicy{
-		Policy:      policyObj,
-		invalidated: false,
-		parents:     parents,
-		children:    children,
-	}
-
-	ok := db.cache.Set(dbIDPolicy+policyObj.Mrn, obj, 2)
-	if !ok {
-		return wrapPolicy{}, errors.New("failed to save policy '" + policyObj.Mrn + "' to cache")
-	}
-
-	list, err := db.listPolicies()
-	if err != nil {
-		return wrapPolicy{}, err
-	}
-
-	list[policyObj.Mrn] = struct{}{}
-	ok = db.cache.Set(dbIDListPolicies, list, 0)
-	if !ok {
-		return wrapPolicy{}, errors.New("failed to update policies list cache")
-	}
-
-	return obj, db.checkAndInvalidatePolicyBundle(ctx, &obj)
-}
-
-func (db *Db) checkAndInvalidatePolicyBundle(ctx context.Context, wrap *wrapPolicy) error {
-	x, ok := db.cache.Get(dbIDBundle + wrap.Policy.Mrn)
-	if !ok {
-		return db.invalidatePolicyAndBundleAncestors(ctx, wrap)
-	}
-
-	bundleObj := x.(wrapBundle)
-	if bundleObj.graphContentChecksum == wrap.Policy.GraphContentChecksum {
-		log.Trace().Str("policy", wrap.Policy.Mrn).Msg("marketplace> policy cache is up-to-date")
-		return nil
-	}
-
-	return db.invalidatePolicyAndBundleAncestors(ctx, wrap)
-}
-
-func (db *Db) invalidatePolicyAndBundleAncestors(ctx context.Context, wrap *wrapPolicy) error {
-	mrn := wrap.Policy.Mrn
-	log.Debug().Str("policy", mrn).Msg("invalidate policy cache")
-
-	// invalidate the policy if its not invalided
-	if wrap.invalidated == false {
-		wrap.invalidated = true
-		db.cache.Set(dbIDPolicy+mrn, *wrap, 2)
-	}
-
-	x, ok := db.cache.Get(dbIDBundle + mrn)
-	if ok {
-		wrapB := x.(wrapBundle)
-
-		// invalidate the policy bundle if its not invalided
-		if wrapB.invalidated == false {
-			wrapB.invalidated = true
-			db.cache.Set(dbIDBundle+mrn, wrapB, 3)
-		}
-	}
-
-	// update all dependencies
-	for parentMrn := range wrap.parents {
-		x, ok := db.cache.Get(dbIDPolicy + parentMrn)
-		if !ok {
-			return errors.New("policy '" + mrn + "' not found")
-		}
-		parent := x.(wrapPolicy)
-
-		if err := db.invalidatePolicyAndBundleAncestors(ctx, &parent); err != nil {
-			return err
-		}
-	}
-
 	return nil
 }
