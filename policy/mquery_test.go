cf0c691ded403ecd30f7e77be6d3ac8c66b026b2
Dominik Richter <dominik.richter@gmail.com>
2022-10-09 23:44:06 -0700

    ✨ mqueries.RefreshChecksums + migrate tests (#59)
    Also exposes a few more methods we want to share on the interface.
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ policy/mquery_test.go
@@ -0,0 +1,45 @@
+package policy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMquery_Whitespaces(t *testing.T) {
+	mq := Mquery{
+		Query: "  mondoo { version \n}   \t\n  ",
+	}
+
+	mqexpect := Mquery{
+		Query: "mondoo { version \n}",
+	}
+
+	bundle, err := mq.RefreshChecksumAndType(nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, bundle)
+
+	bundle, err = mqexpect.RefreshChecksumAndType(nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, bundle)
+
+	assert.Equal(t, mqexpect.CodeId, mq.CodeId)
+}
+
+func TestMquery_CodeIDs(t *testing.T) {
+	mqAssetFilter := Mquery{
+		Query: "mondoo { version \n}",
+	}
+
+	mqReg := Mquery{
+		Query: "mondoo { version \n}",
+	}
+
+	_, err := mqAssetFilter.RefreshAsAssetFilter("//some.mrn")
+	assert.NoError(t, err)
+
+	_, err = mqReg.RefreshChecksumAndType(nil)
+	assert.NoError(t, err)
+
+	assert.Equal(t, mqReg.CodeId, mqAssetFilter.CodeId)
+}
