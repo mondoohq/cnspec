aec77be04fb073603d7040182550645c852db74a
Dominik Richter <dominik.richter@gmail.com>
2022-10-14 00:42:41 -0700

    ⭐ scan report --json
    Introduces a custom printer to create reports. It doesn't just take the raw data and dump it all to CLI, but instead carefully constructs a JSON representation of the report that is useful for further processing. It contains MRNs for queries, eliminates pesky lookups, and nicely aggregates data.
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ cli/reporter/json.go
@@ -0,0 +1,125 @@
+package reporter
+
+import (
+	"encoding/json"
+	"errors"
+	"strconv"
+
+	cr "go.mondoo.com/cnquery/cli/reporter"
+	"go.mondoo.com/cnquery/llx"
+	"go.mondoo.com/cnquery/shared"
+	"go.mondoo.com/cnspec/policy"
+)
+
+func ReportCollectionToJSON(data *policy.ReportCollection, out shared.OutputHelper) error {
+	if data == nil {
+		return nil
+	}
+
+	queryMrnIdx := map[string]string{}
+	for i := range data.Bundle.Queries {
+		query := data.Bundle.Queries[i]
+		queryMrnIdx[query.CodeId] = query.Mrn
+	}
+
+	out.WriteString(
+		"{" +
+			"\"assets\":")
+	assets, err := json.Marshal(data.Assets)
+	if err != nil {
+		return err
+	}
+	out.WriteString(string(assets))
+
+	out.WriteString("," +
+		"\"data\":" +
+		"{")
+	pre := ""
+	for id, report := range data.Reports {
+		out.WriteString(pre + llx.PrettyPrintString(id) + ":{")
+		pre = ","
+
+		resolved, ok := data.ResolvedPolicies[id]
+		if !ok {
+			return errors.New("cannot find resolved pack for " + id + " in report")
+		}
+
+		results := report.RawResults()
+		pre2 := ""
+		for qid, query := range resolved.ExecutionJob.Queries {
+			mrn := queryMrnIdx[qid]
+			// policies and other stuff
+			if mrn == "" {
+				continue
+			}
+			// controls
+			if _, ok := report.Scores[qid]; ok {
+				continue
+			}
+
+			out.WriteString(pre2 + llx.PrettyPrintString(mrn) + ":")
+			pre2 = ","
+
+			err := cr.BundleResultsToJSON(query.Code, results, out)
+			if err != nil {
+				return err
+			}
+		}
+		out.WriteString("}")
+	}
+
+	out.WriteString("}," +
+		"\"scores\":" +
+		"{")
+	pre = ""
+	for id, report := range data.Reports {
+		out.WriteString(pre + llx.PrettyPrintString(id) + ":{")
+		pre = ","
+
+		resolved, ok := data.ResolvedPolicies[id]
+		if !ok {
+			return errors.New("cannot find resolved pack for " + id + " in report")
+		}
+
+		pre2 := ""
+		for qid := range resolved.ExecutionJob.Queries {
+			mrn := queryMrnIdx[qid]
+			// policies and other stuff
+			if mrn == "" {
+				continue
+			}
+			// controls
+			score, ok := report.Scores[qid]
+			if !ok {
+				continue
+			}
+
+			status := score.TypeLabel()
+			if score.Type == policy.ScoreType_Result {
+				if score.Value == 100 {
+					status = "pass"
+				} else {
+					status = "fail"
+				}
+			}
+
+			out.WriteString(pre2 + llx.PrettyPrintString(mrn) +
+				":{\"score\":" + strconv.FormatUint(uint64(score.Value), 10) + "," +
+				"\"status\":\"" + status + "\"}")
+			pre2 = ","
+		}
+		out.WriteString("}")
+	}
+
+	out.WriteString("}," +
+		"\"errors\":" +
+		"{")
+	pre = ""
+	for id, err := range data.Errors {
+		out.WriteString(pre + llx.PrettyPrintString(id) + ":" + llx.PrettyPrintString(err))
+		pre = ","
+	}
+	out.WriteString("}}")
+
+	return nil
+}
