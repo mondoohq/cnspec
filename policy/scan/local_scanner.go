740cc0dd3aa9a9214cde8ac36601c004c29a1483
Christoph Hartmann <chris@lollyrock.com>
2023-02-07 00:26:33 +0100

    ⭐️ use new cnquery creds resolver

--- policy/scan/local_scanner.go
+++ policy/scan/local_scanner.go
@@ -178,7 +178,7 @@ func (s *LocalScanner) RunIncognito(ctx context.Context, job *Job) (*ScanResult,
 
 func (s *LocalScanner) distributeJob(job *Job, ctx context.Context, upstreamConfig resources.UpstreamConfig) (*ScanResult, bool, error) {
 	log.Info().Msgf("discover related assets for %d asset(s)", len(job.Inventory.Spec.Assets))
-	im, err := inventory.New(inventory.WithInventory(job.Inventory))
+	im, err := inventory.New(inventory.WithInventory(job.Inventory), inventory.WithCachedCredsResolver())
 	if err != nil {
 		return nil, false, errors.Wrap(err, "could not load asset information")
 	}
@@ -269,7 +269,7 @@ func (s *LocalScanner) distributeJob(job *Job, ctx context.Context, upstreamConf
 				PolicyFilters:    job.PolicyFilters,
 				Props:            job.Props,
 				Ctx:              ctx,
-				GetCredential:    im.GetCredential,
+				CredsResolver:    im.CredsResolver,
 				Reporter:         reporter,
 				ProgressReporter: p,
 			})
@@ -303,7 +303,7 @@ func (s *LocalScanner) RunAssetJob(job *AssetJob) {
 	}
 
 	// run over all connections
-	connections, err := resolver.OpenAssetConnections(job.Ctx, job.Asset, job.GetCredential, job.DoRecord)
+	connections, err := resolver.OpenAssetConnections(job.Ctx, job.Asset, job.CredsResolver, job.DoRecord)
 	if err != nil {
 		job.Reporter.AddScanError(job.Asset, err)
 		job.ProgressReporter.Score("X")
