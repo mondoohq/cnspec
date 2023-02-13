e243e396164469281590351bc99327ab04f2e931
Christoph Hartmann <chris@lollyrock.com>
2023-02-13 17:28:45 +0100

    🧹 post-install script of package migrates mondoo service to cnspec service

--- scripts/pkg/linux/postinstall.sh
+++ scripts/pkg/linux/postinstall.sh
@@ -9,12 +9,26 @@ then
   test -n "$(initctl status cnspec | grep -o running)" && \
     echo " -> Restart cnspec service (init)" && \
     initctl restart cnspec
+
+  # if mondoo service is running, stop it and start cnspec
+  test -n "$(initctl status mondoo | grep -o running)" && \
+    echo " -> Replace mondoo service with cnspec service (init)" && \
+    initctl stop mondoo && \
+    initctl start cnspec
+
 elif [ "$(cat /proc/1/comm)" = "systemd" ]
 then
   systemctl is-active --quiet cnspec && \
     echo " -> Restart cnspec service (systemd)" && \
     systemctl daemon-reload && \
     systemctl restart cnspec
+
+  systemctl is-active --quiet mondoo && \
+      echo " -> Replace mondoo service with cnspec service (systemd)" && \
+      systemctl daemon-reload && \
+      systemctl stop mondoo && \
+      systemctl disable mondoo && \
+      systemctl start cnspec
 fi
 
 exit 0
