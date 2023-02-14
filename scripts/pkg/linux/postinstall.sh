d8f4fb6c26569386bd3b52a198fca306b2a6e2b2
Joel Diaz <joel@mondoo.com>
2023-02-14 17:16:09 -0500

    perform mondoo-to-cnspec service migration
    on systemd, when upgrading cnspec, check whether the mondoo.service is
    running/enabled.
    
    If mondoo.service is enabled: disable it and enable cnspec.service
    If mondoo.service is running: stop it and run cnspec.service
    
    Upstart-based system will need some manual migration (namely disabling
    the mondoo service and enabling the cnspec service however that system
    was configured to enable those services. The best we will do is see that
    the mondoo service is running, stop it, and starting the cnspec service
    (no permanent enabling/disabling of the services).
    
    An example where mondoo.service was enabled/running:
    
    [root@fedora joel]# rpm -qa | egrep "mondoo|cnsp"
    mondoo-7.15.1-1.x86_64
    cnspec-7.16.2-1.x86_64
    [root@fedora joel]# systemctl is-active mondoo
    active
    [root@fedora joel]# systemctl is-enabled mondoo
    enabled
    [root@fedora joel]# systemctl is-active cnspec
    inactive
    [root@fedora joel]# systemctl is-enabled cnspec
    disabled
    [root@fedora joel]#  rpm -U --force ./cnspec_v7.15.0-snapshot_linux_amd64.rpm
     -> Prepare for cnspec installation
     -> Migrate cnspec configuration
    → No migration needed.
     -> Mondoo service running, replacing with cnspec (systemd)
     -> Mondoo service enabled, replacing with cnspec (systemd)
    Removed "/etc/systemd/system/multi-user.target.wants/mondoo.service".
    Created symlink
    /etc/systemd/system/multi-user.target.wants/cnspec.service →
    /etc/systemd/system/cnspec.service.
     -> Restart cnspec service (systemd)
    [root@fedora joel]# systemctl is-enabled mondoo
    disabled
    [root@fedora joel]# systemctl is-active mondoo
    inactive
    [root@fedora joel]# systemctl is-enabled cnspec
    enabled
    [root@fedora joel]# systemctl is-active cnspec
    active

--- scripts/pkg/linux/postinstall.sh
+++ scripts/pkg/linux/postinstall.sh
@@ -6,29 +6,40 @@ cnspec --config /etc/opt/mondoo/mondoo.yml migrate
 
 if [ "$(cat /proc/1/comm)" = "init" ]
 then
+  # Upstart best-effort to just stop mondoo service and
+  # start cnspec service. Removing mondoo from upstart
+  # will not be scripted here.
+  # if mondoo service is running, stop it and start cnspec
+  test -n "$(initctl status mondoo | grep -o running)" && \
+    echo " -> Stop mondoo service; start cnspec service (init)" && \
+    initctl stop mondoo && \
+    initctl start cnspec
+
   test -n "$(initctl status cnspec | grep -o running)" && \
     echo " -> Restart cnspec service (init)" && \
     initctl restart cnspec
 
-  # if mondoo service is running, stop it and start cnspec
-  test -n "$(initctl status mondoo | grep -o running)" && \
-    echo " -> Replace mondoo service with cnspec service (init)" && \
-    initctl stop mondoo && \
-    initctl start cnspec
-
 elif [ "$(cat /proc/1/comm)" = "systemd" ]
 then
+  # if Mondoo service is currently running, make sure
+  # cnspec service is running to replace it
+  systemctl is-active --quiet mondoo && \
+    echo " -> Mondoo service running, replacing with cnspec (systemd)" && \
+    systemctl stop mondoo && \
+    systemctl daemon-reload && \
+    systemctl restart cnspec
+
+  # if Mondoo service is set up to run on boot
+  # replace it with cnspec to start on boot
+  systemctl is-enabled --quiet mondoo 2>/dev/null && \
+    echo " -> Mondoo service enabled, replacing with cnspec (systemd)" && \
+    systemctl disable mondoo &&
+    systemctl enable cnspec
+
   systemctl is-active --quiet cnspec && \
     echo " -> Restart cnspec service (systemd)" && \
     systemctl daemon-reload && \
     systemctl restart cnspec
-
-  systemctl is-active --quiet mondoo && \
-      echo " -> Replace mondoo service with cnspec service (systemd)" && \
-      systemctl daemon-reload && \
-      systemctl stop mondoo && \
-      systemctl disable mondoo && \
-      systemctl start cnspec
 fi
 
 exit 0
