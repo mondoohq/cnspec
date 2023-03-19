6865dfeafc1f572d5218ce3a1582d71542803355
Christoph Hartmann <chris@lollyrock.com>
2023-03-19 16:26:17 +0100

    🐛 adjust linting to v8 policy structure

--- internal/bundle/lint_test.go
+++ internal/bundle/lint_test.go
@@ -7,14 +7,36 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestLint(t *testing.T) {
+func TestLintPass(t *testing.T) {
 	file := "../../examples/example.mql.yaml"
 	rootDir := "../../examples"
 	results, err := Lint(file)
 	require.NoError(t, err)
 
 	assert.Equal(t, 1, len(results.BundleLocations))
-	assert.Equal(t, 2, len(results.Entries))
+	assert.Equal(t, 0, len(results.Entries))
+	assert.False(t, results.HasError())
+
+	report, err := results.sarifReport(rootDir)
+	require.NoError(t, err)
+
+	assert.Equal(t, 1, len(report.Runs))
+	assert.Equal(t, len(rules), len(report.Runs[0].Tool.Driver.Rules))
+	assert.Equal(t, 0, len(report.Runs[0].Results))
+
+	data, err := results.ToSarif(rootDir)
+	require.NoError(t, err)
+	assert.True(t, len(data) > 0)
+}
+
+func TestLintFail(t *testing.T) {
+	file := "./testdata/failing_lint.mql.yaml"
+	rootDir := "./testdata"
+	results, err := Lint(file)
+	require.NoError(t, err)
+
+	assert.Equal(t, 1, len(results.BundleLocations))
+	assert.Equal(t, 5, len(results.Entries))
 	assert.True(t, results.HasError())
 
 	report, err := results.sarifReport(rootDir)
@@ -22,7 +44,7 @@ func TestLint(t *testing.T) {
 
 	assert.Equal(t, 1, len(report.Runs))
 	assert.Equal(t, len(rules), len(report.Runs[0].Tool.Driver.Rules))
-	assert.Equal(t, 2, len(report.Runs[0].Results))
+	assert.Equal(t, 5, len(report.Runs[0].Results))
 
 	data, err := results.ToSarif(rootDir)
 	require.NoError(t, err)
