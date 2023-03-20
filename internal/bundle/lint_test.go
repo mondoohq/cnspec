53da75334f48759e900f0876b93a0d90975c2bb8
Dominik Richter <dominik.richter@gmail.com>
2023-03-20 09:06:28 -0700

    🐛 lint query variants
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- internal/bundle/lint_test.go
+++ internal/bundle/lint_test.go
@@ -29,6 +29,28 @@ func TestLintPass(t *testing.T) {
 	assert.True(t, len(data) > 0)
 }
 
+func TestLintComplexPass(t *testing.T) {
+	file := "../../examples/complex.mql.yaml"
+	rootDir := "../../examples"
+	results, err := Lint(file)
+	require.NoError(t, err)
+
+	assert.Equal(t, 1, len(results.BundleLocations))
+	assert.Equal(t, 1, len(results.Entries)) // TODO: one more to fix
+	assert.True(t, results.HasError())       // TODO: one more to fix
+
+	report, err := results.sarifReport(rootDir)
+	require.NoError(t, err)
+
+	assert.Equal(t, 1, len(report.Runs))
+	assert.Equal(t, len(rules), len(report.Runs[0].Tool.Driver.Rules))
+	assert.Equal(t, 1, len(report.Runs[0].Results)) // TODO: one more to fix
+
+	data, err := results.ToSarif(rootDir)
+	require.NoError(t, err)
+	assert.True(t, len(data) > 0)
+}
+
 func TestLintFail(t *testing.T) {
 	file := "./testdata/failing_lint.mql.yaml"
 	rootDir := "./testdata"
