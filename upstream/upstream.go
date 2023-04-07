8370a2da928e21e5876f0764561ca8604a6b3e27
Christoph Hartmann <chris@lollyrock.com>
2023-04-07 17:35:32 +0200

    🐛 fix proto generation

--- upstream/upstream.go
+++ upstream/upstream.go
@@ -1,3 +1,3 @@
 package upstream
 
-//go:generate protoc --proto_path=../:../cnquery:. --go_out=. --go_opt=paths=source_relative --rangerrpc_out=. upstream.proto
+//go:generate protoc --proto_path=../:../cnquery:. --go_out=. --go_opt=paths=source_relative --rangerrpc_out=. cnspec_upstream.proto
