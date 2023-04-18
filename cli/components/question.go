a47358e54c37ce4a07d925a716a5a688a9f20964
Preslav <preslav@mondoo.com>
2023-04-18 11:11:50 +0300

    🧹 Exit browser report prompt if user specifies anything other than a 'y' or a 'yes'
    Signed-off-by: Preslav <preslav@mondoo.com>

--- cli/components/question.go
+++ cli/components/question.go
@@ -12,20 +12,14 @@ import (
 func AskAYesNoQuestion(msg string) bool {
 	reader := bufio.NewReader(os.Stdin)
 
-	for {
-		fmt.Printf("%s [(y)es/(n)o]: ", msg)
+	fmt.Printf("%s [Y/n] ", msg)
 
-		answer, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to read response")
-		}
-
-		answer = strings.ToLower(strings.TrimSpace(answer))
-
-		if answer == "y" || answer == "yes" {
-			return true
-		} else if answer == "n" || answer == "no" {
-			return false
-		}
+	answer, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to read response")
 	}
+
+	answer = strings.ToLower(strings.TrimSpace(answer))
+
+	return answer == "y" || answer == "yes"
 }
