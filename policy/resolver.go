aad143eb92d24cbb89d28a201383c94f56638b2f
Christian Zunker <christian@mondoo.com>
2023-06-01 13:22:01 +0200

    ✨ Add exceptions to FrameworkGroup and PolicyGroup
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- policy/resolver.go
+++ policy/resolver.go
@@ -892,13 +892,21 @@ func (s *LocalServices) policyGroupToJobs(ctx context.Context, group *PolicyGrou
 			continue
 		}
 
+		validUntil := time.Unix(group.EndDate, 0).Format(time.RFC3339)
+		if check.Action == explorer.Action_IGNORE {
+			stillValid := CheckValidUntil(validUntil, check.Mrn)
+			if !stillValid {
+				// the exception is no longer valid => score the check
+				check.Action = explorer.Action_ACTIVATE
+			}
+		}
 		// If we ignore this check, we have to transfer this info to the impact var,
 		// which is used to inform how to aggregate the scores of all child jobs.
 		impact := check.Impact
+		if impact == nil {
+			impact = &explorer.Impact{}
+		}
 		if check.Action == explorer.Action_IGNORE {
-			if impact == nil {
-				impact = &explorer.Impact{}
-			}
 			impact.Scoring = explorer.ScoringSystem_IGNORE_SCORE
 			impact.Action = check.Action
 		}
@@ -1385,6 +1393,41 @@ func ensureControlJob(cache *frameworkResolverCache, jobs map[string]*ReportingJ
 		return found
 	}
 
+	assetFramework := cache.bundleMap.Frameworks[framework.Mrn]
+
+	var control *Control
+	var controlGroup *FrameworkGroup
+	for i := range assetFramework.Dependencies {
+		depFramework := cache.bundleMap.Frameworks[assetFramework.Dependencies[i].Mrn]
+		for j := range depFramework.Groups {
+			group := depFramework.Groups[j]
+			if group.Type != GroupType_IGNORED {
+				continue
+			}
+			for k := range group.Controls {
+				if group.Controls[k].Mrn == controlMrn {
+					control = group.Controls[k]
+					controlGroup = group
+				}
+			}
+		}
+	}
+
+	// If we ignore this control, we have to transfer this info to the impact var,
+	// which is used to inform how to aggregate the results of all child jobs.
+	impact := &explorer.Impact{}
+	validUntil := ""
+	if controlGroup != nil && controlGroup.EndDate != 0 {
+		time.Unix(controlGroup.EndDate, 0).Format(time.RFC3339)
+	}
+	if control != nil && controlGroup.Action == explorer.Action_IGNORE {
+		stillIgnore := CheckValidUntil(validUntil, control.Mrn)
+		if stillIgnore {
+			impact.Scoring = explorer.ScoringSystem_IGNORE_SCORE
+			impact.Action = explorer.Action_IGNORE
+		}
+	}
+
 	controlJob := &ReportingJob{
 		Uuid:          uuid,
 		QrId:          controlMrn,
@@ -1403,8 +1446,7 @@ func ensureControlJob(cache *frameworkResolverCache, jobs map[string]*ReportingJ
 			continue
 		}
 
-		// FIXME: we need to take user exceptions into account!
-		frameworkJob.ChildJobs[uuid] = &explorer.Impact{}
+		frameworkJob.ChildJobs[uuid] = impact
 		controlJob.Notify = append(controlJob.Notify, parentUuid)
 	}
 
@@ -1527,4 +1569,28 @@ func (s *LocalServices) updateAssetJobs(ctx context.Context, assetMrn string, as
 	}
 
 	return s.DataLake.SetAssetResolvedPolicy(ctx, assetMrn, resolvedPolicy, V2Code)
+}
+
+// CheckValidUntil returns whether the given time is lying in the future or not.
+// Specialcase is an empty string, which is treated as forever.
+func CheckValidUntil(validUntil string, mrn string) bool {
+	stillIgnore := false
+	// empty validUntil means ignore forever
+	if validUntil == "" {
+		stillIgnore = true
+		log.Debug().Str("mrn", mrn).Msg("control is ignored forever")
+	} else {
+		validTime, err := time.Parse(time.RFC3339, validUntil)
+		if err != nil {
+			// user wanted an exception, but something went wrong with the date
+			// should we bubble up the error?
+			log.Error().Err(err).Str("mrn", mrn).Msg("failed to parse validUntil")
+		} else {
+			if validTime.After(time.Now()) {
+				stillIgnore = true
+				log.Debug().Str("mrn", mrn).Msg("is ignored for now because of validUntil timestamp")
+			}
+		}
+	}
+	return stillIgnore
 }
--- policy/resolver_test.go
+++ policy/resolver_test.go
@@ -473,3 +473,94 @@ func (tester *frameworkReportingJobTester) requireReportsTo(childQueryId string,
 		require.Equal(tester.t, policy.ReportingJob_CHECK, childRj.Type)
 	}
 }
+
+func TestResolve_CheckValidUntil(t *testing.T) {
+	stillValid := policy.CheckValidUntil(time.Now().Format(time.RFC3339), "test123")
+	require.False(t, stillValid)
+	stillValid = policy.CheckValidUntil(time.Now().Add(time.Hour*1).Format(time.RFC3339), "test123")
+	require.True(t, stillValid)
+	// wrong format as input, should return false
+	stillValid = policy.CheckValidUntil(time.Now().Format(time.RFC1123), "test123")
+	require.False(t, stillValid)
+}
+
+func TestResolve_ControlActionIgnore(t *testing.T) {
+	b := parseBundle(t, `
+owner_mrn: //test.sth
+policies:
+- uid: ssh-policy
+  name: SSH Policy
+  groups:
+  - filters: "true"
+    checks:
+    - uid: sshd-ciphers-01
+      title: Prevent weaker CBC ciphers from being used
+      mql: sshd.config.ciphers.none( /cbc/ )
+      impact: 60
+    - uid: sshd-ciphers-02
+      title: Do not allow ciphers with few bits
+      mql: sshd.config.ciphers.none( /128/ )
+      impact: 60
+      action: 4
+      valid_until: 2020-12-31T23:59:59+00:00
+    - uid: sshd-config-permissions
+      title: SSH config editing should be limited to admins
+      mql: sshd.config.file.permissions.mode == 0644
+      impact: 100
+
+frameworks:
+- uid: mondoo-ucf
+  name: Unified Compliance Framework
+  groups:
+  - title: System hardening
+    controls:
+    - uid: mondoo-ucf-01
+      title: Only use strong ciphers
+      checks:
+      - uid: sshd-ciphers-01
+      - uid: sshd-ciphers-02        
+    - uid: mondoo-ucf-02
+      title: Limit access to system configuration
+      checks:
+      - uid: sshd-config-permissions
+  - title: exception-1
+    type: 4
+    action: 4
+    controls:
+    - uid: mondoo-ucf-02
+`)
+
+	_, srv, err := inmemory.NewServices(nil)
+	require.NoError(t, err)
+
+	_, err = srv.SetBundle(context.Background(), b)
+	require.NoError(t, err)
+
+	_, err = srv.Assign(context.Background(), &policy.PolicyAssignment{
+		AssetMrn:      "asset1",
+		PolicyMrns:    []string{policyMrn("ssh-policy")},
+		FrameworkMrns: []string{"//test.sth/framework/mondoo-ucf"},
+	})
+	require.NoError(t, err)
+
+	filters, err := srv.GetPolicyFilters(context.Background(), &policy.Mrn{Mrn: "asset1"})
+	require.NoError(t, err)
+	assetPolicy, err := srv.GetPolicy(context.Background(), &policy.Mrn{Mrn: "asset1"})
+	require.NoError(t, err)
+
+	err = srv.DataLake.SetPolicy(context.Background(), assetPolicy, filters.Items)
+	require.NoError(t, err)
+
+	t.Run("resolve with ignored control", func(t *testing.T) {
+		rp, err := srv.Resolve(context.Background(), &policy.ResolveReq{
+			PolicyMrn:    "asset1",
+			AssetFilters: []*explorer.Mquery{{Mql: "true"}},
+		})
+		require.NoError(t, err)
+		require.NotNil(t, rp)
+		require.Len(t, rp.CollectorJob.ReportingJobs, 8)
+		frameworkJob := rp.CollectorJob.ReportingJobs["wzI27buJ4+E="]
+		require.Equal(t, frameworkJob.Type, policy.ReportingJob_FRAMEWORK)
+		require.Equal(t, explorer.ScoringSystem_IGNORE_SCORE, frameworkJob.ChildJobs["AD/0ooourC4="].Scoring)
+	})
+}
