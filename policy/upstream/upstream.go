6558f78e8b5f5a7b29bcc5352667a7c27cdc775a
Dominik Richter <dominik.richter@gmail.com>
2023-09-29 10:06:56 -0700

    ✨ migrate --share-report to v9
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ policy/upstream/upstream.go
@@ -0,0 +1,32 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package upstream
+
+//go:generate protoc --proto_path=../../:../../cnquery:. --go_out=. --go_opt=paths=source_relative --rangerrpc_out=. cnspec_upstream.proto
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/rs/zerolog/log"
+	"go.mondoo.com/cnspec/policy"
+	"go.mondoo.com/ranger-rpc"
+)
+
+const sharedReportUrl = "https://report.api.mondoo.com"
+
+func UploadSharedReport(report *policy.ReportCollection, reportUrl string, proxy *url.URL) (*ReportID, error) {
+	if reportUrl == "" {
+		reportUrl = sharedReportUrl
+	}
+
+	httpClient := ranger.NewHttpClient(ranger.WithProxy(proxy))
+	sharedReportClient, err := NewReportingClient(reportUrl, httpClient)
+	if err != nil {
+		log.Error().Err(err).Msg("error initializing shared report client")
+		return nil, err
+	}
+
+	return sharedReportClient.StoreReport(context.Background(), report)
+}
