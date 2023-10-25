c662bbec30d3cb58b74aafa60ce8b45e10268fbf
Preslav Gerchev <preslavgerchevmail@gmail.com>
2023-10-25 20:20:02 +0300

    🐛 Fix adding ignored checks in the resolver (#888)
    * Correctly ignore checks when their action is set to IGNORE. Remove deprecated v7 code.
    
    * Add test for disabled query. Simplify validUntil and make sure it doesnt reactive queries if no enddate.
    
    * small fix.
    
    ---------
    
    Co-authored-by: Victoria Jeffrey <vj@mondoo.com>

--- policy/resolver.go
+++ policy/resolver.go
@@ -529,9 +529,6 @@ func (s *LocalServices) tryResolve(ctx context.Context, bundleMrn string, assetF
 		ChildJobs:  map[string]*explorer.Impact{},
 		Datapoints: map[string]bool{},
 		Type:       ReportingJob_POLICY,
-		// FIXME: DEPRECATED, remove in v9.0 vv
-		DeprecatedV7Spec: map[string]*DeprecatedV7_ScoringSpec{},
-		// ^^
 	}
 
 	cache.reportingJobsByUUID[reportingJob.Uuid] = reportingJob
@@ -822,9 +819,6 @@ func (s *LocalServices) policyGroupToJobs(ctx context.Context, group *PolicyGrou
 					Datapoints:    map[string]bool{},
 					ScoringSystem: policyObj.ScoringSystem,
 					Type:          ReportingJob_POLICY,
-					// FIXME: DEPRECATED, remove in v9.0 vv
-					DeprecatedV7Spec: map[string]*DeprecatedV7_ScoringSpec{},
-					// ^^
 				}
 				cache.global.reportingJobsByUUID[policyJob.Uuid] = policyJob
 				cache.childJobsByMrn[policy.Mrn] = []*ReportingJob{policyJob}
@@ -839,9 +833,6 @@ func (s *LocalServices) policyGroupToJobs(ctx context.Context, group *PolicyGrou
 			// local aspects for the resolved policy
 			policyJob.Notify = append(policyJob.Notify, ownerJob.Uuid)
 			ownerJob.ChildJobs[policyJob.Uuid] = impact
-			// FIXME: DEPRECATED, remove in v9.0 vv
-			ownerJob.DeprecatedV7Spec[policyJob.Uuid] = Impact2ScoringSpec(impact)
-			// ^^
 
 			if err := s.policyToJobs(ctx, policy.Mrn, policyJob, cache, now); err != nil {
 				return err
@@ -868,9 +859,6 @@ func (s *LocalServices) policyGroupToJobs(ctx context.Context, group *PolicyGrou
 					parentJob := cache.global.reportingJobsByUUID[id]
 					if parentJob != nil {
 						parentJob.ChildJobs[policyJob.Uuid] = impact
-						// FIXME: DEPRECATED, remove in v9.0 vv
-						parentJob.DeprecatedV7Spec[policyJob.Uuid] = Impact2ScoringSpec(impact)
-						// ^^
 					}
 				}
 			}
@@ -903,9 +891,8 @@ func (s *LocalServices) policyGroupToJobs(ctx context.Context, group *PolicyGrou
 			continue
 		}
 
-		validUntil := time.Unix(group.EndDate, 0).Format(time.RFC3339)
 		if check.Action == explorer.Action_IGNORE {
-			stillValid := CheckValidUntil(validUntil, check.Mrn)
+			stillValid := CheckValidUntil(group.EndDate, check.Mrn)
 			if !stillValid {
 				// the exception is no longer valid => score the check
 				check.Action = explorer.Action_ACTIVATE
@@ -919,7 +906,7 @@ func (s *LocalServices) policyGroupToJobs(ctx context.Context, group *PolicyGrou
 		}
 		if check.Action == explorer.Action_IGNORE {
 			impact.Scoring = explorer.ScoringSystem_IGNORE_SCORE
-			impact.Action = check.Action
+			impact.Action = explorer.Action_IGNORE
 		}
 
 		cache.global.propsCache.Add(check.Props...)
@@ -929,7 +916,8 @@ func (s *LocalServices) policyGroupToJobs(ctx context.Context, group *PolicyGrou
 			continue
 		}
 
-		if check.Action == explorer.Action_MODIFY {
+		// TODO: can we simplify this to simply IGNORE?
+		if check.Action == explorer.Action_MODIFY || check.Action == explorer.Action_IGNORE {
 			cache.modifyCheckJob(check, impact)
 		}
 	}
@@ -984,9 +972,6 @@ func (cache *policyResolverCache) addCheckJob(ctx context.Context, check *explor
 			ChildJobs:  map[string]*explorer.Impact{},
 			Datapoints: map[string]bool{},
 			Type:       ReportingJob_CHECK,
-			// FIXME: DEPRECATED, remove in v9.0 vv
-			DeprecatedV7Spec: map[string]*DeprecatedV7_ScoringSpec{},
-			// ^^
 		}
 		cache.global.codeIdToMrn[check.CodeId] = append(cache.global.codeIdToMrn[check.CodeId], check.Mrn)
 		cache.global.reportingJobsByUUID[uuid] = queryJob
@@ -998,9 +983,6 @@ func (cache *policyResolverCache) addCheckJob(ctx context.Context, check *explor
 	queryJob.Notify = append(queryJob.Notify, ownerJob.Uuid)
 
 	ownerJob.ChildJobs[queryJob.Uuid] = impact
-	// FIXME: DEPRECATED, remove in v9.0 vv
-	ownerJob.DeprecatedV7Spec[queryJob.Uuid] = Impact2ScoringSpec(impact)
-	// ^^
 
 	if len(check.Variants) != 0 {
 		err := cache.addCheckJobVariants(ctx, check, queryJob)
@@ -1069,10 +1051,6 @@ func (cache *policyResolverCache) addDataQueryJob(ctx context.Context, query *ex
 			Type:       ReportingJob_DATA_QUERY,
 			// FIXME: DEPRECATED, remove in v10.0 vv
 			DeprecatedV8IsData: true,
-			// ^^
-			// FIXME: DEPRECATED, remove in v9.0 vv
-			DeprecatedV7Spec: map[string]*DeprecatedV7_ScoringSpec{},
-			// ^^
 		}
 		cache.global.reportingJobsByUUID[queryJob.Uuid] = queryJob
 		cache.global.reportingJobsByMsum[query.Checksum] = append(cache.global.reportingJobsByMsum[query.Checksum], queryJob)
@@ -1139,9 +1117,6 @@ func (cache *policyResolverCache) modifyCheckJob(check *explorer.Mquery, impact 
 			parentJob := cache.global.reportingJobsByUUID[id]
 			if parentJob != nil {
 				parentJob.ChildJobs[queryJob.Uuid] = impact
-				// FIXME: DEPRECATED, remove in v9.0 vv
-				parentJob.DeprecatedV7Spec[queryJob.Uuid] = Impact2ScoringSpec(impact)
-				// ^^
 			}
 		}
 	}
@@ -1422,13 +1397,9 @@ func ensureControlJob(cache *frameworkResolverCache, jobs map[string]*ReportingJ
 	// If we ignore this control, we have to transfer this info to the impact var,
 	// which is used to inform how to aggregate the results of all child jobs.
 	impact := &explorer.Impact{}
-	validUntil := ""
 	if frameworkGroup, ok := frameworkGroupByControlMrn[controlMrn]; ok {
 		if frameworkGroup.Type == GroupType_IGNORED {
-			if frameworkGroup.EndDate != 0 {
-				validUntil = time.Unix(frameworkGroup.EndDate, 0).Format(time.RFC3339)
-			}
-			stillIgnore := CheckValidUntil(validUntil, controlMrn)
+			stillIgnore := CheckValidUntil(frameworkGroup.EndDate, controlMrn)
 			if stillIgnore {
 				impact.Scoring = explorer.ScoringSystem_IGNORE_SCORE
 				impact.Action = explorer.Action_IGNORE
@@ -1712,25 +1683,19 @@ func (s *LocalServices) updateAssetJobs(ctx context.Context, assetMrn string, as
 	return s.DataLake.SetAssetResolvedPolicy(ctx, assetMrn, resolvedPolicy, V2Code)
 }
 
-// CheckValidUntil returns whether the given time is lying in the future or not.
-// Specialcase is an empty string, which is treated as forever.
-func CheckValidUntil(validUntil string, mrn string) bool {
+// CheckValidUntil returns whether the given time is laying in the future or not.
+// Special case is a unix ts of 0, which is treated as forever.
+func CheckValidUntil(validUntil int64, mrn string) bool {
 	stillIgnore := false
 	// empty validUntil means ignore forever
-	if validUntil == "" {
+	if validUntil == 0 {
 		stillIgnore = true
 		log.Debug().Str("mrn", mrn).Msg("control is ignored forever")
 	} else {
-		validTime, err := time.Parse(time.RFC3339, validUntil)
-		if err != nil {
-			// user wanted an exception, but something went wrong with the date
-			// should we bubble up the error?
-			log.Error().Err(err).Str("mrn", mrn).Msg("failed to parse validUntil")
-		} else {
-			if validTime.After(time.Now()) {
-				stillIgnore = true
-				log.Debug().Str("mrn", mrn).Msg("is ignored for now because of validUntil timestamp")
-			}
+		validTime := time.Unix(validUntil, 0)
+		if validTime.After(time.Now()) {
+			stillIgnore = true
+			log.Debug().Str("mrn", mrn).Msg("is ignored for now because of validUntil timestamp")
 		}
 	}
 	return stillIgnore
