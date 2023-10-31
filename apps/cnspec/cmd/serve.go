e6a250dc98ede72f99b4754a9445ec4f8ebf146c
Christian Zunker <christian@mondoo.com>
2023-10-31 09:07:17 +0100

    Remove health pinger
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- apps/cnspec/cmd/serve.go
+++ apps/cnspec/cmd/serve.go
@@ -80,10 +80,6 @@ var serveCmd = &cobra.Command{
 				return cli_errors.NewCommandError(errors.Wrap(err, "could not initialize upstream client"), 1)
 			}
 
-			hc := backgroundjob.NewHealthPinger(ctx, client.HttpClient, client.ApiEndpoint, 5*time.Minute)
-			hc.Start()
-			defer hc.Stop()
-
 			checkin := backgroundjob.NewCheckinPinger(ctx, client.HttpClient, client.ApiEndpoint, conf.runtime.UpstreamConfig, 2*time.Hour)
 			checkin.Start()
 			defer checkin.Stop()
