0f5f78ab7336580add4df12d7b767a91c4303767
Ivan Milchev <ivan@mondoo.com>
2023-12-21 12:46:04 +0200

    remove unnecessary code
    Signed-off-by: Ivan Milchev <ivan@mondoo.com>

--- policy/frameworks_test.go
+++ policy/frameworks_test.go
@@ -5,71 +5,7 @@ package policy
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
-
-func TestResolveFramework_Exceptions(t *testing.T) {
-	frameworks := map[string]*Framework{
-		"framework-mrn1": {
-			Mrn: "framework-mrn1",
-			Groups: []*FrameworkGroup{
-				{
-					Controls: []*Control{
-						{Mrn: "control-mrn1"},
-						{Mrn: "control-mrn2"},
-						{Mrn: "control-mrn3"},
-						{Mrn: "control-mrn4"},
-					},
-				},
-				{
-					Type: GroupType_DISABLE,
-					Controls: []*Control{
-						{Mrn: "control-mrn1"},
-					},
-				},
-				{
-					Type: GroupType_OUT_OF_SCOPE,
-					Controls: []*Control{
-						{Mrn: "control-mrn2"},
-					},
-				},
-				{
-					Type:         GroupType_DISABLE,
-					ReviewStatus: ReviewStatus_REJECTED,
-					Controls: []*Control{
-						{Mrn: "control-mrn3"},
-					},
-				},
-				{
-					Type:         GroupType_OUT_OF_SCOPE,
-					ReviewStatus: ReviewStatus_REJECTED,
-					Controls: []*Control{
-						{Mrn: "control-mrn4"},
-					},
-				},
-			},
-			FrameworkMaps: []*FrameworkMap{
-				{
-					Controls: []*ControlMap{
-						{Mrn: "control-mrn1"},
-						{Mrn: "control-mrn2"},
-						{Mrn: "control-mrn3"},
-						{Mrn: "control-mrn4"},
-					},
-				},
-			},
-		},
-	}
-
-	resolved := ResolveFramework("framework-mrn1", frameworks)
-	assert.NotNil(t, resolved)
-	assert.Len(t, resolved.ReportTargets, 2)
-	assert.NotContains(t, resolved.ReportTargets, "control-mrn1")
-	assert.NotContains(t, resolved.ReportTargets, "control-mrn2")
-	assert.Contains(t, resolved.ReportTargets, "control-mrn3")
-	assert.Contains(t, resolved.ReportTargets, "control-mrn4")
-}
 
 func TestResolvedFrameworkTopologicalSort(t *testing.T) {
 	framework := &ResolvedFramework{
