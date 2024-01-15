cee35d0ff80f6206b61a0ef2ccf6438d26373b7a
Christoph Hartmann <chris@lollyrock.com>
2024-01-15 11:00:44 +0100

    ⭐️ panic handler to report panics

--- apps/cnspec/cnspec.go
+++ apps/cnspec/cnspec.go
@@ -3,8 +3,13 @@
 
 package main
 
-import "go.mondoo.com/cnspec/v10/apps/cnspec/cmd"
+import (
+	"go.mondoo.com/cnquery/v10/providers-sdk/v1/upstream/health"
+	"go.mondoo.com/cnspec/v10"
+	"go.mondoo.com/cnspec/v10/apps/cnspec/cmd"
+)
 
 func main() {
+	defer health.ReportPanic("cnspec", cnspec.Version, cnspec.Build)
 	cmd.Execute()
 }
