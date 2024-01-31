82d5560adaca036320a1cd7a7e10bc0d4d177f10
Christoph Hartmann <chris@lollyrock.com>
2024-01-31 21:41:23 +0100

    🧹 provides a function to generate formatted bundle from a policy.Bundle (#1093)

---
+++ policy/fmt/fmt_test.go
@@ -0,0 +1,55 @@
+// Copyright (c) Mondoo, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package fmt
+
+import (
+	"github.com/stretchr/testify/require"
+	"go.mondoo.com/cnspec/v10/policy"
+	"testing"
+)
+
+func TestBundleFormattedYaml(t *testing.T) {
+	bundleStr := `
+  owner_mrn: //test.sth
+  policies:
+  - uid: policy1
+    groups:
+    - filters: "true"
+      checks:
+      - uid: check-1
+        mql: 1 == 2
+      - uid: check-2
+        mql: failme.name != ""
+      queries:
+      - uid: query-1
+        mql: 1 == 1
+      - mql: failme.name
+        uid: query-2
+`
+
+	bundle, err := policy.BundleFromYAML([]byte(bundleStr))
+	require.NoError(t, err)
+	require.NotNil(t, bundle)
+
+	formatted, err := ToFormattedYAML(bundle)
+	require.NoError(t, err)
+
+	expectedStr := `owner_mrn: //test.sth
+policies:
+  - uid: policy1
+    groups:
+      - filters: "true"
+        checks:
+          - uid: check-1
+            mql: 1 == 2
+          - uid: check-2
+            mql: failme.name != ""
+        queries:
+          - uid: query-1
+            mql: 1 == 1
+          - uid: query-2
+            mql: failme.name
+`
+	require.Equal(t, expectedStr, string(formatted))
+}
