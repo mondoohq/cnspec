a80ef6a5d5261c101e0e97c8f5e4bd0c2e243511
Christoph Hartmann <chris@lollyrock.com>
2024-02-19 17:29:35 +0100

    🧹 harmonize the version handling for mondoo.version

--- apps/cnspec/cmd/shell.go
+++ apps/cnspec/cmd/shell.go
@@ -7,11 +7,9 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"go.mondoo.com/cnquery/v10"
 	cnquery_app "go.mondoo.com/cnquery/v10/apps/cnquery/cmd"
 	"go.mondoo.com/cnquery/v10/providers"
 	"go.mondoo.com/cnquery/v10/providers-sdk/v1/plugin"
-	"go.mondoo.com/cnspec/v10"
 )
 
 func init() {
@@ -33,13 +31,6 @@ var shellCmd = &cobra.Command{
 var shellRun = func(cmd *cobra.Command, runtime *providers.Runtime, cliRes *plugin.ParseCLIRes) {
 	shellConf := cnquery_app.ParseShellConfig(cmd, cliRes)
 	shellConf.WelcomeMessage = cnspecLogo
-
-	// FIXME: workaround for `mondoo.version` in case of builtin providers
-	// (which is how the core provider is set up by default)
-	if cnquery.Version == "" {
-		cnquery.Version = cnspec.Version
-	}
-
 	if err := cnquery_app.StartShell(runtime, shellConf); err != nil {
 		log.Fatal().Err(err).Msg("failed to run query")
 	}
