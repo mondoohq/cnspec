f8c19cb0a134b5e3351cebfd694b1bb251256a52
Preslav <preslav@mondoo.com>
2024-04-04 19:49:30 +0300

    🧹 Modify logger for serve instead of logging timestamps manually.
    Signed-off-by: Preslav <preslav@mondoo.com>

--- apps/cnspec/cmd/backgroundjob/serve_windows.go
+++ apps/cnspec/cmd/backgroundjob/serve_windows.go
@@ -62,12 +62,12 @@ func (m *windowsService) Execute(args []string, r <-chan svc.ChangeRequest, chan
 		// that's not possible right now and requires wiring through
 		// context throughout the execution.
 		for range runChan {
-			log.Info().Time("now", time.Now().UTC()).Msg("starting background scan")
+			log.Info().Msg("starting background scan")
 			err := m.Handler()
 			if err != nil {
 				log.Error().Err(err).Send()
 			} else {
-				log.Info().Time("now", time.Now().UTC()).Msg("scan completed")
+				log.Info().Msg("scan completed")
 			}
 		}
 	}()
@@ -85,15 +85,14 @@ loop:
 				splayDur = time.Duration(rand.Int63n(int64(m.Splay)))
 			}
 			nextRun := m.Timer + splayDur
-			n := time.Now().UTC()
-			log.Info().Time("now", n).Time("next scan", n.Add(nextRun)).Msgf("next scan in %v", nextRun)
+			log.Info().Time("next scan", time.Now().Add(nextRun)).Msgf("next scan in %v", nextRun)
 			t.Reset(nextRun)
 		case c := <-r:
 			switch c.Cmd {
 			case svc.Interrogate:
 				changes <- c.CurrentStatus
 			case svc.Stop, svc.Shutdown:
-				log.Info().Time("now", time.Now().UTC()).Msg("stopping cnspec service")
+				log.Info().Msg("stopping cnspec service")
 				break loop
 			default:
 				log.Error().Msgf("unexpected control request #%d", c)
@@ -108,7 +107,7 @@ loop:
 func runService(isDebug bool, timer time.Duration, splay time.Duration, handler JobRunner) {
 	var err error
 
-	log.Info().Time("now", time.Now().UTC()).Msgf("starting %s service", SvcName)
+	log.Info().Msgf("starting %s service", SvcName)
 	run := svc.Run
 	if isDebug {
 		run = debug.Run
@@ -122,5 +121,5 @@ func runService(isDebug bool, timer time.Duration, splay time.Duration, handler 
 		log.Info().Msgf("%s service failed: %v", SvcName, err)
 		return
 	}
-	log.Info().Time("now", time.Now().UTC()).Msgf("%s service stopped", SvcName)
+	log.Info().Msgf("%s service stopped", SvcName)
 }
