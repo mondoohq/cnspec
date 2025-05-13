12fa263e134d17d86be27f2170374097b773f47b
Dimitar Ganev <dimitar@mondoo.com>
2025-05-13 11:44:07 +0300

    Add linter tests

--- internal/bundle/lint_test.go
+++ internal/bundle/lint_test.go
@@ -32,7 +32,7 @@ func TestResults_SarifReport(t *testing.T) {
 	assert.Equal(t, 0, len(report.Runs[0].Results))
 }
 
-func TestLintPass(t *testing.T) {
+func TestLinter_Pass(t *testing.T) {
 	file := "./testdata/pass_linter.yaml"
 	results, err := bundle.Lint(schema, file)
 	require.NoError(t, err)
@@ -45,10 +45,172 @@ func TestLinter_Fail_PolicyUidRuleID(t *testing.T) {
 	file := "./testdata/fail_PolicyUidRuleID.yaml"
 	results, err := bundle.Lint(schema, file)
 	require.NoError(t, err)
-	assert.Equal(t, 1, len(results.BundleLocations))
 	assert.Equal(t, 2, len(results.Entries))
 	assert.Equal(t, "policy 'Ubuntu Benchmark 1' (at line 3) does not define a UID", results.Entries[0].Message)
 	assert.Equal(t, "policy-uid", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
 	assert.Equal(t, "Could not compile policy bundle: failed to refresh policy : failed to refresh mrn for policy Ubuntu Benchmark 1 : cannot refresh MRN with an empty UID", results.Entries[1].Message)
 	assert.Equal(t, "bundle-compile-error", results.Entries[1].RuleID)
+}
+
+func TestLinter_Fail_PolicyNameRuleID(t *testing.T) {
+	file := "./testdata/fail_PolicyNameRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(results.Entries))
+	assert.Equal(t, "policy 'ubuntu-bench-1' does not define a name", results.Entries[0].Message)
+	assert.Equal(t, "policy-name", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
+}
+
+func TestLinter_Fail_PolicyMissingAssetFilterRuleID_Variant(t *testing.T) {
+	file := "./testdata/fail_noFiltersGroupAndCheckVariant.mql.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(results.Entries))
+	assert.Equal(t, "policy 'mondoo-aws-security', group 'AWS IAM' (line 20): Check 'mondoo-aws-security-access-keys-rotated' lacks an asset filter or variants, and the group also has no filter.", results.Entries[0].Message)
+	assert.Equal(t, "policy-missing-asset-filter", results.Entries[0].RuleID)
+	assert.Equal(t, "warning", results.Entries[0].Level)
+}
+
+func TestLinter_Fail_PolicyMissingAssetFilterRuleID(t *testing.T) {
+	file := "./testdata/fail_noFiltersGroupAndCheck.mql.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(results.Entries))
+	assert.Equal(t, "policy 'mondoo-aws-security', group 'AWS IAM' (line 20): Check 'mondoo-aws-security-access-keys-rotated' lacks an asset filter or variants, and the group also has no filter.", results.Entries[0].Message)
+	assert.Equal(t, "policy-missing-asset-filter", results.Entries[0].RuleID)
+	assert.Equal(t, "warning", results.Entries[0].Level)
+}
+
+func TestLinter_Fail_PolicyUidUniqueRuleID(t *testing.T) {
+	file := "./testdata/fail_PolicyUidUniqueRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(results.Entries))
+	assert.Equal(t, "Global query UID 'ubuntu-hard-2-1' is used multiple times in the same file", results.Entries[0].Message)
+	assert.Equal(t, "query-uid-unique", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
+}
+
+func TestLinter_Fail_PolicyMissingChecksRuleID(t *testing.T) {
+	file := "./testdata/fail_PolicyMissingChecksRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 5, len(results.Entries))
+	assert.Equal(t, "policy 'ubuntu-bench-1', group 'Configure Ubuntu 1' (line 16) has no checks, data queries, or sub-policies defined", results.Entries[0].Message)
+	assert.Equal(t, "policy-missing-checks", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
+	assert.Equal(t, "Global query UID 'ubuntu-1-1' is defined but not assigned to any policy", results.Entries[1].Message)
+	assert.Equal(t, "query-unassigned", results.Entries[1].RuleID)
+	assert.Equal(t, "warning", results.Entries[1].Level)
+}
+
+func TestLinter_Fail_PolicyMissingVersionRuleID(t *testing.T) {
+	file := "./testdata/fail_PolicyMissingVersionRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(results.Entries))
+	assert.Equal(t, "policy 'ubuntu-bench-1' is missing version", results.Entries[0].Message)
+	assert.Equal(t, "policy-missing-version", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
+}
+
+func TestLinter_Fail_PolicyWrongVersionRuleID(t *testing.T) {
+	file := "./testdata/fail_PolicyWrongVersionRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 2, len(results.Entries))
+	assert.Equal(t, "policy 'ubuntu-bench-1' has invalid version 'hehe.1.2.3.4': Invalid Semantic Version", results.Entries[0].Message)
+	assert.Equal(t, "policy-wrong-version", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
+	assert.Equal(t, "Could not compile policy bundle: failed to validate policy: policy '//local.cnspec.io/run/local-execution/policies/ubuntu-bench-1' version 'hehe.1.2.3.4' is not a valid semver version", results.Entries[1].Message)
+	assert.Equal(t, "bundle-compile-error", results.Entries[1].RuleID)
+	assert.Equal(t, "error", results.Entries[1].Level)
+}
+
+func TestLinter_Fail_PolicyRequiredTagsMissingRuleID(t *testing.T) {
+	file := "./testdata/fail_PolicyRequiredTagsMissingRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 2, len(results.Entries))
+	assert.Equal(t, "policy 'ubuntu-bench-1' does not contain the required tag `mondoo.com/category`", results.Entries[0].Message)
+	assert.Equal(t, "policy-required-tags-missing", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
+	assert.Equal(t, "policy 'ubuntu-bench-1' does not contain the required tag `mondoo.com/platform`", results.Entries[1].Message)
+	assert.Equal(t, "policy-required-tags-missing", results.Entries[1].RuleID)
+	assert.Equal(t, "error", results.Entries[1].Level)
+}
+
+func TestLinter_Fail_QueryUidRuleID(t *testing.T) {
+	file := "./testdata/fail_QueryUidRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(results.Entries))
+	assert.Equal(t, "Global query UID 'ubuntu-1-1' is used multiple times in the same file", results.Entries[0].Message)
+	assert.Equal(t, "query-uid-unique", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
+}
+
+func TestLinter_Fail_QueryTitleRuleID(t *testing.T) {
+	file := "./testdata/fail_QueryTitleRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(results.Entries))
+	assert.Equal(t, "Global query 'ubuntu-hard-2-2' does not define a title", results.Entries[0].Message)
+	assert.Equal(t, "query-name", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
+}
+
+func TestLinter_Fail_QueryVariantUsesNonDefaultFieldsRuleID(t *testing.T) {
+	file := "./testdata/fail_QueryVariantUsesNonDefaultFieldsRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(results.Entries))
+	assert.Equal(t, "Query variant 'ubuntu-hard-2-1-var1' must not define 'impact'", results.Entries[0].Message)
+	assert.Equal(t, "query-variant-uses-non-default-fields", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
+}
+
+func TestLinter_Fail_QueryMissingMQLRuleID(t *testing.T) {
+	file := "./testdata/fail_QueryMissingMQLRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 2, len(results.Entries))
+	assert.Equal(t, "Global query 'ubuntu-hard-2-2' has no variants and must define MQL", results.Entries[0].Message)
+	assert.Equal(t, "query-missing-mql", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
+	assert.Equal(t, "Could not compile policy bundle: failed to validate query '//local.cnspec.io/run/local-execution/queries/ubuntu-hard-2-2': failed to compile query '': query is not implemented '//local.cnspec.io/run/local-execution/queries/ubuntu-hard-2-2'\n", results.Entries[1].Message)
+	assert.Equal(t, "bundle-compile-error", results.Entries[1].RuleID)
+	assert.Equal(t, "error", results.Entries[1].Level)
+}
+
+func TestLinter_Fail_QueryUnassignedRuleID(t *testing.T) {
+	file := "./testdata/fail_QueryUnassignedRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(results.Entries))
+	assert.Equal(t, "Global query UID 'ubuntu-hard-1-1' is defined but not assigned to any policy", results.Entries[0].Message)
+	assert.Equal(t, "query-unassigned", results.Entries[0].RuleID)
+	assert.Equal(t, "warning", results.Entries[0].Level)
+}
+
+func TestLinter_QueryUsedAsDifferentTypesRuleID(t *testing.T) {
+	file := "./testdata/fail_QueryUsedAsDifferentTypesRuleID.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(results.Entries))
+	assert.Equal(t, "Query UID 'sshd-sshd-01' is used as both a check and a data query in policies", results.Entries[0].Message)
+	assert.Equal(t, "query-used-as-different-types", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
+}
+
+func TestLinter_BundleUnknownField(t *testing.T) {
+	file := "./testdata/fail_BundleUnknownField.yaml"
+	results, err := bundle.Lint(schema, file)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(results.Entries))
+	assert.Equal(t, "Bundle file fail_BundleUnknownField.yaml contains unknown fields: error unmarshaling JSON: while decoding JSON: json: unknown field \"unknown_field\"", results.Entries[0].Message)
+	assert.Equal(t, "bundle-unknown-field", results.Entries[0].RuleID)
+	assert.Equal(t, "error", results.Entries[0].Level)
 }
