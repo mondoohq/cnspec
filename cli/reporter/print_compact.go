mondoohq/cnspec

commit cbbcdc0de62701a48f13f7571f847a552bbe749d

⭐ valid until (#1754)
* ⭐ valid until

Introduces the `valid` keyword in policies, which supports setting an
`until` value. This allows us to create human-readable policy groups
that are configured for a limited time.

This is particularly useful when defining temporary exceptions:

```
policies:
  - uid: example1
    name: Example policy 1
    groups:
      - filters:
          - mql: asset.family.contains('unix')
        checks:
          - uid: check-05
            title: SSHd should only use very secure ciphers
            mql: |
              sshd.config.ciphers.all( _ == /ctr/ )
            impact: 95

      - type: override
        title: Exception for strong ciphers until September
        valid:
          until: 2025-09-01
        checks:
          - uid: check-05
            action: preview
```

Depends on https://github.com/mondoohq/cnquery/pull/5817

* 🧹 fix genai mistakes

Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

* 🧹 linter suggestion

* jays changes

* update recalculateAt

* update cnquery

* update policy checksums

* fix tests

---------

Signed-off-by: Dominik Richter <dominik.richter@gmail.com>
Co-authored-by: Jay Mundrawala <jay@mondoo.com>

cli/reporter/print_compact.go
@@ -6,12 +6,15 @@ package reporter
 import (
 	"fmt"
 	"io"
+	"math"
 	"regexp"
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 	"unicode/utf8"
 
+	"github.com/mergestat/timediff"
 	"github.com/muesli/termenv"
 	"github.com/rs/zerolog/log"
 	"go.mondoo.com/cnquery/v11/explorer"
@@ -207,9 +210,19 @@ func (r *defaultReporter) printAssetSections(orderedAssets []assetMrnName) {
 
 	var queries map[string]*explorer.Mquery
 	var controls map[string]*policy.Control
+	previewChecks := map[string]*policy.PolicyGroup{}
 	if r.bundle != nil {
 		queries = r.bundle.QueryMap()
 		controls = r.bundle.ControlsMap()
+		for _, p := range r.bundle.Policies {
+			for _, g := range p.Groups {
+				for _, c := range g.Checks {
+					if c.Action == explorer.Action_IGNORE || g.Type == policy.GroupType_IGNORED {
+						previewChecks[c.Mrn] = g
+					}
+				}
+			}
+		}
 	}
 
 	for _, assetMrnName := range orderedAssets {
@@ -250,7 +263,7 @@ func (r *defaultReporter) printAssetSections(orderedAssets []assetMrnName) {
 		}
 
 		if r.Conf.printData || r.Conf.printChecks {
-			r.printAssetQueries(resolved, report, queries, assetMrn, asset)
+			r.printAssetQueries(resolved, report, queries, previewChecks, assetMrn, asset)
 		}
 
 		if r.Conf.printRisks {
@@ -361,7 +374,12 @@ type simpleScore struct {
 	Rating  policy.ScoreRating
 }
 
-func (r *defaultReporter) printAssetQueries(resolved *policy.ResolvedPolicy, report *policy.Report, queries map[string]*explorer.Mquery, assetMrn string, asset *inventory.Asset) {
+type previewGroup struct {
+	until          int64
+	sortedFailures []string
+}
+
+func (r *defaultReporter) printAssetQueries(resolved *policy.ResolvedPolicy, report *policy.Report, queries map[string]*explorer.Mquery, checkToPreview map[string]*policy.PolicyGroup, assetMrn string, asset *inventory.Asset) {
 	results := report.RawResults()
 
 	if r.Conf.printData {
@@ -385,54 +403,70 @@ func (r *defaultReporter) printAssetQueries(resolved *policy.ResolvedPolicy, rep
 		}
 	}
 
+	previewGroups := map[int]*previewGroup{
+		math.MaxInt64: {until: math.MaxInt64},
+	}
+	for _, pg := range checkToPreview {
+		until := math.MaxInt64
+		if pg.Valid != nil && pg.Valid.Until != nil {
+			until = int(pg.Valid.Until.Seconds)
+		}
+		previewGroups[until] = &previewGroup{}
+	}
+
 	if r.Conf.printChecks {
 		foundChecks := map[string]simpleScore{}
 		sortedPassed := []string{}
 		sortedWarnings := []string{}
 		sortedFailed := []string{}
 
-		for id, score := range report.Scores {
+		for id, query := range queries {
 			_, ok := resolved.CollectorJob.ReportingQueries[id]
 			if !ok {
 				continue
 			}
 
-			query, ok := queries[id]
+			pscore, ok := report.Scores[query.Mrn]
 			if !ok {
 				r.out("Couldn't find any queries for score of " + id)
 				continue
 			}
 
 			// FIXME v12: this is only a workaround for a deeper bug with the score value
+			isSuccess := pscore.Value == 100
 			if query.Impact != nil && query.Impact.Value != nil {
 				floor := 100 - uint32(query.Impact.Value.Value)
-				if floor > score.Value {
-					score.Value = floor
+				if floor > pscore.Value {
+					pscore.Value = floor
 				}
 			}
 
 			score := simpleScore{
-				Value:   score.Value,
-				Type:    score.Type,
-				Message: score.Message,
-				Rating:  score.Rating(),
+				Value:   pscore.Value,
+				Type:    pscore.Type,
+				Message: pscore.Message,
+				Rating:  pscore.Rating(),
 				// FIXME v12: this is incorrect because the score value is 100 for failing checks whose impact is 0
-				Success: score.Value == 100,
+				Success: isSuccess,
 			}
 			foundChecks[id] = score
 
 			if score.Success {
 				sortedPassed = append(sortedPassed, id)
-			} else if score.Value >= uint32(r.ScoreThreshold) {
+			} else if score.Value >= uint32(r.ScoreThreshold) && r.ScoreThreshold != 0 {
 				sortedWarnings = append(sortedWarnings, id)
+			} else if score.Type == policy.ScoreType_Skip {
+				g := checkToPreview[query.Mrn]
+				var pg *previewGroup
+				if g != nil && g.Valid != nil && g.Valid.Until != nil {
+					pg = previewGroups[int(g.Valid.Until.Seconds)]
+				} else {
+					pg = previewGroups[math.MaxInt64]
+				}
+				pg.sortedFailures = append(pg.sortedFailures, id)
 			} else {
 				sortedFailed = append(sortedFailed, id)
 			}
-		}
-
-		if r.ScoreThreshold == 0 {
-			sortedFailed = append(sortedFailed, sortedWarnings...)
-			sortedWarnings = []string{}
 		}
 
 		sort.Slice(sortedPassed, func(i, j int) bool {
@@ -479,6 +513,48 @@ func (r *defaultReporter) printAssetQueries(resolved *policy.ResolvedPolicy, rep
 			for _, id := range sortedWarnings {
 				r.printCheck(foundChecks[id], queries[id], resolved, report, results)
 			}
+			prevPrinted = true
+		}
+
+		times := []int{}
+		for t, pg := range previewGroups {
+			if len(pg.sortedFailures) != 0 {
+				times = append(times, t)
+			}
+		}
+		sort.Ints(times)
+		for i := len(times) - 1; i >= 0; i-- {
+			if prevPrinted {
+				r.out(NewLineCharacter)
+			}
+
+			unixTime := times[i]
+			deadline := time.Unix(int64(unixTime), 0)
+			diff := timediff.TimeDiff(deadline)
+			r.out("Remediation deadline: " + deadline.Format("2006/01/02") + " (" + diff + ")\n")
+
+			group := previewGroups[unixTime]
+			sort.Slice(group.sortedFailures, func(i, j int) bool {
+				ida := group.sortedFailures[i]
+				idb := group.sortedFailures[j]
+				a := foundChecks[ida].Value
+				b := foundChecks[idb].Value
+				if a == b {
+					return queries[ida].Title < queries[idb].Title
+				}
+				return a > b
+			})
+
+			for _, id := range group.sortedFailures {
+				// we have to force the score type for the printer here,
+				// so we can see it as a failure and not as a skip
+				score := foundChecks[id]
+				score.Type = policy.ScoreType_Result
+				ps := policy.Score{Value: score.Value, Type: score.Type, ScoreCompletion: 100}
+				score.Rating = ps.Rating()
+				r.printCheck(score, queries[id], resolved, report, results)
+			}
+
 			prevPrinted = true
 		}
 

policy/executor/internal/nodes.go
@@ -447,10 +447,11 @@ func (nodeData *ReportingJobNodeData) consume(from NodeID, data *envelope) {
 				// We map errors to failed results.
 				// Skip and unknown are mapped to passing results
 				score = score.CloneVT()
-				if score.Type == policy.ScoreType_Error {
+				switch score.Type {
+				case policy.ScoreType_Error:
 					score.Type = policy.ScoreType_Result
 					score.Value = 0
-				} else if score.Type == policy.ScoreType_Skip || score.Type == policy.ScoreType_Unscored {
+				case policy.ScoreType_Skip, policy.ScoreType_Unscored:
 					score.Type = policy.ScoreType_Result
 					score.Value = 100
 				}
@@ -546,6 +547,7 @@ func (nodeData *ReportingJobNodeData) score() (*policy.Score, error) {
 			} else {
 				s = c.score.CloneVT()
 				s.QrId = nodeData.queryID
+
 				if c.impact.GetScoring() == explorer.ScoringSystem_DISABLED {
 					s.Type = policy.ScoreType_Disabled
 				} else if s.Type == policy.ScoreType_Result {
@@ -557,6 +559,12 @@ func (nodeData *ReportingJobNodeData) score() (*policy.Score, error) {
 							if floor > s.Value {
 								s.Value = floor
 							}
+						}
+
+						// since we clone it from the child, which is just the raw score,
+						// we have to set the type on this layer
+						if c.impact.Scoring == explorer.ScoringSystem_IGNORE_SCORE {
+							s.Type = policy.ScoreType_Skip
 						}
 					}
 				}

cli/reporter/cli_reporter_test.go
@@ -42,10 +42,8 @@ func TestCompactReporter(t *testing.T) {
 	rr.print()
 
 	strData := buf.String()
-	assert.Contains(t, strData, "✕ HIGH (30):      Ensure")
-	assert.Contains(t, strData, ". Skipped:        Set")
 	assert.Contains(t, strData, "! Error:          Set")
-	assert.Contains(t, strData, "✓ Ensure rsyslog is installed")
+	assert.Contains(t, strData, "✓ Ensure ")
 	assert.Contains(t, strData, "✕ CRITICAL (0):   Ensure")
 }
 

cli/reporter/file_handler_test.go
@@ -37,10 +37,8 @@ func TestFileHandler(t *testing.T) {
 		require.NoError(t, err)
 
 		strData := string(data)
-		assert.Contains(t, strData, "✕ HIGH (30):      Ensure")
-		assert.Contains(t, strData, ". Skipped:        Set")
 		assert.Contains(t, strData, "! Error:          Set")
-		assert.Contains(t, strData, "✓ Ensure rsyslog is installed")
+		assert.Contains(t, strData, "✓ Ensure ")
 		assert.Contains(t, strData, "✕ CRITICAL (0):   Ensure")
 		err = os.Remove(fileName)
 		require.NoError(t, err)
@@ -58,10 +56,8 @@ func TestFileHandler(t *testing.T) {
 		require.NoError(t, err)
 
 		strData := string(data)
-		assert.Contains(t, strData, "✕ HIGH (30):      Ensure")
-		assert.Contains(t, strData, ". Skipped:        Set")
 		assert.Contains(t, strData, "! Error:          Set")
-		assert.Contains(t, strData, "✓ Ensure rsyslog is installed")
+		assert.Contains(t, strData, "✓ Ensure ")
 		assert.Contains(t, strData, "✕ CRITICAL (0):   Ensure")
 		err = os.Remove(trimmed)
 		require.NoError(t, err)

cli/reporter/proto_test.go
@@ -27,16 +27,18 @@ func TestProtoConversion(t *testing.T) {
 
 		assert.Equal(t, 1, len(report.Assets))
 
-		assetMrn := "//assets.api.mondoo.app/spaces/dazzling-golick-767384/assets/2DRZ1cCWFyTYCArycAXHwvn1oU2"
+		assetMrn := "//assets.api.mondoo.app/spaces/test-infallible-taussig-796596/assets/310qEuaqCbVCMU5tGa1HbLx8TZc"
+		require.Contains(t, assetMrn, assetMrn)
+
 		asset := report.Assets[assetMrn]
-		assert.Equal(t, "X1", asset.Name)
+		assert.Equal(t, "ubuntu:24.04", asset.Name)
 		assert.Equal(t, "ubuntu", asset.PlatformName)
 
 		assert.Equal(t, 1, len(report.Scores))
 		assert.Equal(t, 0, len(report.Errors))
 		assert.Equal(t, 1, len(report.Data))
 
-		assert.Equal(t, 108, len(report.Scores[assetMrn].Values))
+		assert.Equal(t, 53, len(report.Scores[assetMrn].Values))
 
 		score := report.Scores[assetMrn].Values["//policy.api.mondoo.app/queries/mondoo-linux-security-permissions-on-etcgshadow-are-configured"]
 		assert.Equal(t, 100, int(score.Score))

go.mod
@@ -28,6 +28,7 @@ require (
 	github.com/hashicorp/terraform-json v0.25.0
 	github.com/jstemmer/go-junit-report/v2 v2.1.0
 	github.com/mattn/go-isatty v0.0.20
+	github.com/mergestat/timediff v0.0.4
 	github.com/muesli/reflow v0.3.0
 	github.com/muesli/termenv v0.16.0
 	github.com/olekukonko/tablewriter v0.0.5
@@ -42,12 +43,12 @@ require (
 	github.com/spf13/viper v1.20.1
 	github.com/stretchr/testify v1.10.0
 	github.com/zclconf/go-cty v1.16.3
-	go.mondoo.com/cnquery/v11 v11.66.1
+	go.mondoo.com/cnquery/v11 v11.66.2-0.20250808140455-f4120cd5c34c
 	go.mondoo.com/mondoo-go v0.0.0-20250702005123-23558fe8001c
 	go.mondoo.com/ranger-rpc v0.7.0
 	go.opentelemetry.io/otel v1.37.0
 	gocloud.dev v0.43.0
-	golang.org/x/sys v0.34.0
+	golang.org/x/sys v0.35.0
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250715232539-7130f93afb79
 	google.golang.org/protobuf v1.36.6
 	gopkg.in/yaml.v3 v3.0.1
@@ -301,16 +302,16 @@ require (
 	go.uber.org/mock v0.5.2 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
-	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b // indirect
-	golang.org/x/mod v0.26.0 // indirect
-	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/mod v0.27.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
-	golang.org/x/term v0.33.0 // indirect
-	golang.org/x/text v0.27.0 // indirect
+	golang.org/x/term v0.34.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
 	golang.org/x/time v0.12.0 // indirect
-	golang.org/x/tools v0.35.0 // indirect
+	golang.org/x/tools v0.36.0 // indirect
 	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
 	google.golang.org/api v0.242.0 // indirect
 	google.golang.org/genproto v0.0.0-20250715232539-7130f93afb79 // indirect

go.sum
@@ -766,6 +766,8 @@ github.com/mattn/go-tty v0.0.3/go.mod h1:ihxohKRERHTVzN+aSVRwACLCeqIoZAWpoICkkvr
 github.com/mattn/go-tty v0.0.7 h1:KJ486B6qI8+wBO7kQxYgmmEFDaFEE96JMBQ7h400N8Q=
 github.com/mattn/go-tty v0.0.7/go.mod h1:f2i5ZOvXBU/tCABmLmOfzLz9azMo5wdAaElRNnJKr+k=
 github.com/matttproud/golang_protobuf_extensions v1.0.1/go.mod h1:D8He9yQNgCq6Z5Ld7szi9bcBfOoFv/3dc6xSMkL2PC0=
+github.com/mergestat/timediff v0.0.4 h1:NZ3sqG/6K9flhTubdltmRx3RBfIiYv6LsGP+4FlXMM8=
+github.com/mergestat/timediff v0.0.4/go.mod h1:yvMUaRu2oetc+9IbPLYBJviz6sA7xz8OXMDfhBl7YSI=
 github.com/miekg/dns v1.0.14/go.mod h1:W1PPwlIAgtquWBMBEV9nkV9Cazfe8ScdGz/Lj7v3Nrg=
 github.com/miekg/dns v1.1.26/go.mod h1:bPDLeHnStXmXAq1m/Ch/hvfNHr14JKNPMBo3VZKjuso=
 github.com/miekg/dns v1.1.41/go.mod h1:p6aan82bvRIyn+zDIv9xYNUpwa73JcSh9BKwknJysuI=
@@ -1033,8 +1035,8 @@ github.com/zeebo/errs v1.4.0/go.mod h1:sgbWHsvVuTPHcqJJGQ1WhI5KbWlHYz+2+2C/LSEtC
 go.etcd.io/etcd/api/v3 v3.5.1/go.mod h1:cbVKeC6lCfl7j/8jBhAK6aIYO9XOjdptoxU/nLQcPvs=
 go.etcd.io/etcd/client/pkg/v3 v3.5.1/go.mod h1:IJHfcCEKxYu1Os13ZdwCwIUTUVGYTSAM3YSwc9/Ac1g=
 go.etcd.io/etcd/client/v2 v2.305.1/go.mod h1:pMEacxZW7o8pg4CrFE7pquyCJJzZvkvdD2RibOCCCGs=
-go.mondoo.com/cnquery/v11 v11.66.1 h1:AHBTFwCCr9aJGO8lZzvBSdYxDOO3XDYNVf/GwbElRjE=
-go.mondoo.com/cnquery/v11 v11.66.1/go.mod h1:6rfarvzViSZsU+8N358cw2EF83tVPK7aUdF7Jj2DM1A=
+go.mondoo.com/cnquery/v11 v11.66.2-0.20250808140455-f4120cd5c34c h1:uE6hsv/tM4n/+Ax7GDDQaMe1cVy3OJuZ35+T8W1dlnI=
+go.mondoo.com/cnquery/v11 v11.66.2-0.20250808140455-f4120cd5c34c/go.mod h1:6rfarvzViSZsU+8N358cw2EF83tVPK7aUdF7Jj2DM1A=
 go.mondoo.com/mondoo-go v0.0.0-20250702005123-23558fe8001c h1:jiKGJYLW9rN6AOXicQmQSgVcZ+SZhM55NwPOfsBsUS8=
 go.mondoo.com/mondoo-go v0.0.0-20250702005123-23558fe8001c/go.mod h1:Ih8FsSC1VhLk7F3hS7Ji6nleAi29VMqRb7f555jr6SE=
 go.mondoo.com/ranger-rpc v0.7.0 h1:QZDgQ8y5e9sCS5BYjwaow1HWBfQ3UwIdfUEQ2SJRO58=
@@ -1108,8 +1110,8 @@ golang.org/x/crypto v0.18.0/go.mod h1:R0j02AL6hcrfOiy9T4ZYp/rcWeMxM3L6QYxlOuEG1m
 golang.org/x/crypto v0.19.0/go.mod h1:Iy9bg/ha4yyC70EfRS8jz+B6ybOBKMaSxLj6P6oBDfU=
 golang.org/x/crypto v0.23.0/go.mod h1:CKFgDieR+mRhux2Lsu27y0fO304Db0wZe70UKqHu0v8=
 golang.org/x/crypto v0.31.0/go.mod h1:kDsLvtWBEx7MV9tJOj9bnXsPbxwJQ6csT/x4KIN4Ssk=
-golang.org/x/crypto v0.40.0 h1:r4x+VvoG5Fm+eJcxMaY8CQM7Lb0l1lsmjGBQ6s8BfKM=
-golang.org/x/crypto v0.40.0/go.mod h1:Qr1vMER5WyS2dfPHAlsOj01wgLbsyWtFn/aY+5+ZdxY=
+golang.org/x/crypto v0.41.0 h1:WKYxWedPGCTVVl5+WHSSrOBT0O8lx32+zxmHxijgXp4=
+golang.org/x/crypto v0.41.0/go.mod h1:pO5AFd7FA68rFak7rOAGVuygIISepHftHnr8dr6+sUc=
 golang.org/x/exp v0.0.0-20190121172915-509febef88a4/go.mod h1:CJ0aWSM057203Lf6IL+f9T1iT9GByDxfZKAQTCR3kQA=
 golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8/go.mod h1:CJ0aWSM057203Lf6IL+f9T1iT9GByDxfZKAQTCR3kQA=
 golang.org/x/exp v0.0.0-20190510132918-efd6b22b2522/go.mod h1:ZjyILWgesfNpC6sMxTJOJm9Kp84zZh5NQWvqDGG3Qr8=
@@ -1154,8 +1156,8 @@ golang.org/x/mod v0.12.0/go.mod h1:iBbtSCu2XBx23ZKBPSOrRkjjQPZFPuis4dIYUhu/chs=
 golang.org/x/mod v0.14.0/go.mod h1:hTbmBsO62+eylJbnUtE2MGJUyE7QWk4xUqPFrRgJ+7c=
 golang.org/x/mod v0.15.0/go.mod h1:hTbmBsO62+eylJbnUtE2MGJUyE7QWk4xUqPFrRgJ+7c=
 golang.org/x/mod v0.17.0/go.mod h1:hTbmBsO62+eylJbnUtE2MGJUyE7QWk4xUqPFrRgJ+7c=
-golang.org/x/mod v0.26.0 h1:EGMPT//Ezu+ylkCijjPc+f4Aih7sZvaAr+O3EHBxvZg=
-golang.org/x/mod v0.26.0/go.mod h1:/j6NAhSk8iQ723BGAUyoAcn7SlD7s15Dp9Nd/SfeaFQ=
+golang.org/x/mod v0.27.0 h1:kb+q2PyFnEADO2IEF935ehFUXlWiNjJWtRNgBLSfbxQ=
+golang.org/x/mod v0.27.0/go.mod h1:rWI627Fq0DEoudcK+MBkNkCe0EetEaDSwJJkCcjpazc=
 golang.org/x/net v0.0.0-20180724234803-3673e40ba225/go.mod h1:mL1N/T3taQHkDXs73rZJwtUhF3w3ftmwwsq0BUmARs4=
 golang.org/x/net v0.0.0-20180826012351-8a410e7b638d/go.mod h1:mL1N/T3taQHkDXs73rZJwtUhF3w3ftmwwsq0BUmARs4=
 golang.org/x/net v0.0.0-20181023162649-9b4f9f5ad519/go.mod h1:mL1N/T3taQHkDXs73rZJwtUhF3w3ftmwwsq0BUmARs4=
@@ -1206,8 +1208,8 @@ golang.org/x/net v0.15.0/go.mod h1:idbUs1IY1+zTqbi8yxTbhexhEEk5ur9LInksu6HrEpk=
 golang.org/x/net v0.20.0/go.mod h1:z8BVo6PvndSri0LbOE3hAn0apkU+1YvI6E70E9jsnvY=
 golang.org/x/net v0.21.0/go.mod h1:bIjVDfnllIU7BJ2DNgfnXvpSvtn8VRwhlsaeUTyUS44=
 golang.org/x/net v0.25.0/go.mod h1:JkAGAh7GEvH74S6FOH42FLoXpXbE/aqXSrIQjXgsiwM=
-golang.org/x/net v0.42.0 h1:jzkYrhi3YQWD6MLBJcsklgQsoAcw89EcZbJw8Z614hs=
-golang.org/x/net v0.42.0/go.mod h1:FF1RA5d3u7nAYA4z2TkclSCKh68eSXtiFwcWQpPXdt8=
+golang.org/x/net v0.43.0 h1:lat02VYK2j4aLzMzecihNvTlJNQUq316m2Mr9rnM6YE=
+golang.org/x/net v0.43.0/go.mod h1:vhO1fvI4dGsIjh73sWfUVjj3N7CA9WkKJNQm2svM6Jg=
 golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be/go.mod h1:N/0e6XlmueqKjAGxoOufVs8QHGRruUQn6yWY3a++T0U=
 golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421/go.mod h1:gOpvHmFTYa4IltrdGE7lF6nIHvwfUNPOp7c8zoXwtLw=
 golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45/go.mod h1:gOpvHmFTYa4IltrdGE7lF6nIHvwfUNPOp7c8zoXwtLw=
@@ -1332,8 +1334,8 @@ golang.org/x/sys v0.16.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
 golang.org/x/sys v0.17.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
 golang.org/x/sys v0.20.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
 golang.org/x/sys v0.28.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
-golang.org/x/sys v0.34.0 h1:H5Y5sJ2L2JRdyv7ROF1he/lPdvFsd0mJHFw2ThKHxLA=
-golang.org/x/sys v0.34.0/go.mod h1:BJP2sWEmIv4KK5OTEluFJCKSidICx8ciO85XgH3Ak8k=
+golang.org/x/sys v0.35.0 h1:vz1N37gP5bs89s7He8XuIYXpyY0+QlsKmzipCbUtyxI=
+golang.org/x/sys v0.35.0/go.mod h1:BJP2sWEmIv4KK5OTEluFJCKSidICx8ciO85XgH3Ak8k=
 golang.org/x/telemetry v0.0.0-20240228155512-f48c80bd79b2/go.mod h1:TeRTkGYfJXctD9OcfyVLyj2J3IxLnKwHJR8f4D8a3YE=
 golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1/go.mod h1:bj7SfCRtBDWHUb9snDiAeCFNEtKQo2Wmx5Cou7ajbmo=
 golang.org/x/term v0.0.0-20210927222741-03fcf44c2211/go.mod h1:jbD1KX2456YbFQfuXm/mYQcufACuNUgVhRMnK/tPxf8=
@@ -1344,8 +1346,8 @@ golang.org/x/term v0.16.0/go.mod h1:yn7UURbUtPyrVJPGPq404EukNFxcm/foM+bV/bfcDsY=
 golang.org/x/term v0.17.0/go.mod h1:lLRBjIVuehSbZlaOtGMbcMncT+aqLLLmKrsjNrUguwk=
 golang.org/x/term v0.20.0/go.mod h1:8UkIAJTvZgivsXaD6/pH6U9ecQzZ45awqEOzuCvwpFY=
 golang.org/x/term v0.27.0/go.mod h1:iMsnZpn0cago0GOrHO2+Y7u7JPn5AylBrcoWkElMTSM=
-golang.org/x/term v0.33.0 h1:NuFncQrRcaRvVmgRkvM3j/F00gWIAlcmlB8ACEKmGIg=
-golang.org/x/term v0.33.0/go.mod h1:s18+ql9tYWp1IfpV9DmCtQDDSRBUjKaw9M1eAv5UeF0=
+golang.org/x/term v0.34.0 h1:O/2T7POpk0ZZ7MAzMeWFSg6S5IpWd/RXDlM9hgM3DR4=
+golang.org/x/term v0.34.0/go.mod h1:5jC53AEywhIVebHgPVeg0mj8OD3VO9OzclacVrqpaAw=
 golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c/go.mod h1:NqM8EUOU14njkJ3fqMW+pc6Ldnwhi/IjpwHt7yyuwOQ=
 golang.org/x/text v0.3.0/go.mod h1:NqM8EUOU14njkJ3fqMW+pc6Ldnwhi/IjpwHt7yyuwOQ=
 golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2/go.mod h1:NqM8EUOU14njkJ3fqMW+pc6Ldnwhi/IjpwHt7yyuwOQ=
@@ -1361,8 +1363,8 @@ golang.org/x/text v0.13.0/go.mod h1:TvPlkZtksWOMsz7fbANvkp4WM8x/WCo/om8BMLbz+aE=
 golang.org/x/text v0.14.0/go.mod h1:18ZOQIKpY8NJVqYksKHtTdi31H5itFRjB5/qKTNYzSU=
 golang.org/x/text v0.15.0/go.mod h1:18ZOQIKpY8NJVqYksKHtTdi31H5itFRjB5/qKTNYzSU=
 golang.org/x/text v0.21.0/go.mod h1:4IBbMaMmOPCJ8SecivzSH54+73PCFmPWxNTLm+vZkEQ=
-golang.org/x/text v0.27.0 h1:4fGWRpyh641NLlecmyl4LOe6yDdfaYNrGb2zdfo4JV4=
-golang.org/x/text v0.27.0/go.mod h1:1D28KMCvyooCX9hBiosv5Tz/+YLxj0j7XhWjpSUF7CU=
+golang.org/x/text v0.28.0 h1:rhazDwis8INMIwQ4tpjLDzUhx6RlXqZNPEM0huQojng=
+golang.org/x/text v0.28.0/go.mod h1:U8nCwOR8jO/marOQ0QbDiOngZVEBB7MAiitBuMjXiNU=
 golang.org/x/time v0.0.0-20181108054448-85acf8d2951c/go.mod h1:tRJNPiyCQ0inRvYxbN9jk5I+vvW/OXSQhTDSoE431IQ=
 golang.org/x/time v0.0.0-20190308202827-9d24e82272b4/go.mod h1:tRJNPiyCQ0inRvYxbN9jk5I+vvW/OXSQhTDSoE431IQ=
 golang.org/x/time v0.0.0-20191024005414-555d28b269f0/go.mod h1:tRJNPiyCQ0inRvYxbN9jk5I+vvW/OXSQhTDSoE431IQ=
@@ -1427,8 +1429,8 @@ golang.org/x/tools v0.6.0/go.mod h1:Xwgl3UAJ/d3gWutnCtw505GrjyAbvKui8lOU390QaIU=
 golang.org/x/tools v0.13.0/go.mod h1:HvlwmtVNQAhOuCjW7xxvovg8wbNq7LwfXh/k7wXUl58=
 golang.org/x/tools v0.17.0/go.mod h1:xsh6VxdV005rRVaS6SSAf9oiAqljS7UZUacMZ8Bnsps=
 golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d/go.mod h1:aiJjzUbINMkxbQROHiO6hDPo2LHcIPhhQsa9DLh0yGk=
-golang.org/x/tools v0.35.0 h1:mBffYraMEf7aa0sB+NuKnuCy8qI/9Bughn8dC2Gu5r0=
-golang.org/x/tools v0.35.0/go.mod h1:NKdj5HkL/73byiZSJjqJgKn3ep7KjFkBOkR/Hps3VPw=
+golang.org/x/tools v0.36.0 h1:kWS0uv/zsvHEle1LbV5LE8QujrxB3wfQyxHfhOk0Qkg=
+golang.org/x/tools v0.36.0/go.mod h1:WBDiHKJK8YgLHlcQPYQzNCkUxUypCaa5ZegCVutKm+s=
 golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=

internal/bundle/bundle.yac.go
@@ -698,6 +698,55 @@ func (s *GroupType) UnmarshalYAML(node *yaml.Node) error {
 	return errors.New("failed to unmarshal GroupType")
 }
 
+type HumanTime struct {
+	Seconds     int64       `protobuf:"varint,1,opt,name=seconds,proto3" json:"seconds,omitempty" yaml:"seconds,omitempty"`
+	FileContext FileContext `json:"-" yaml:"-"`
+	Comments    Comments    `json:"-" yaml:"-"`
+}
+
+func (x *HumanTime) UnmarshalYAML(node *yaml.Node) error {
+	// prevent recursive calls into UnmarshalYAML with a placeholder type
+	type tmp HumanTime
+	err := node.Decode((*tmp)(x))
+	if err != nil {
+		return err
+	}
+
+	x.FileContext.Column = node.Column
+	x.FileContext.Line = node.Line
+
+	headComment := node.HeadComment
+	if headComment == "" && len(node.Content) > 1 {
+		headComment = node.Content[0].HeadComment
+	}
+
+	lineComment := node.LineComment
+
+	footComment := node.FootComment
+	if footComment == "" && len(node.Content) > 1 {
+		last := len(node.Content) - 1
+		footComment = node.Content[last].FootComment
+	}
+
+	x.Comments.HeadComment = headComment
+	x.Comments.LineComment = lineComment
+	x.Comments.FootComment = footComment
+	return nil
+}
+
+func (d HumanTime) MarshalYAML() (interface{}, error) {
+	type alias HumanTime
+	node := yaml.Node{}
+	err := node.Encode(alias(d))
+	if err != nil {
+		return nil, err
+	}
+	node.HeadComment = d.Comments.HeadComment
+	node.LineComment = d.Comments.LineComment
+	node.FootComment = d.Comments.FootComment
+	return node, nil
+}
+
 type Impact struct {
 	Value       *ImpactValue           `protobuf:"bytes,1,opt,name=value,proto3" json:"value,omitempty" yaml:"value,omitempty"`
 	Scoring     explorer.ScoringSystem `protobuf:"varint,2,opt,name=scoring,proto3,enum=cnquery.explorer.ScoringSystem" json:"scoring,omitempty" yaml:"scoring,omitempty"`
@@ -1297,6 +1346,7 @@ type PolicyGroup struct {
 	StartDate    int64            `protobuf:"varint,21,opt,name=start_date,json=startDate,proto3" json:"start_date,omitempty" yaml:"start_date,omitempty"`
 	EndDate      int64            `protobuf:"varint,22,opt,name=end_date,json=endDate,proto3" json:"end_date,omitempty" yaml:"end_date,omitempty"`
 	ReminderDate int64            `protobuf:"varint,23,opt,name=reminder_date,json=reminderDate,proto3" json:"reminder_date,omitempty" yaml:"reminder_date,omitempty"`
+	Valid        *Validity        `protobuf:"bytes,41,opt,name=valid,proto3" json:"valid,omitempty" yaml:"valid,omitempty"`
 	Reviewers    []*Author        `protobuf:"bytes,27,rep,name=reviewers,proto3" json:"reviewers,omitempty" yaml:"reviewers,omitempty"`
 	ReviewStatus ReviewStatus     `protobuf:"varint,28,opt,name=review_status,json=reviewStatus,proto3,enum=cnspec.policy.v1.ReviewStatus" json:"review_status,omitempty" yaml:"review_status,omitempty"`
 	Created      int64            `protobuf:"varint,32,opt,name=created,proto3" json:"created,omitempty" yaml:"created,omitempty"`
@@ -2077,6 +2127,56 @@ func (x *TypedDoc) UnmarshalYAML(node *yaml.Node) error {
 
 func (d TypedDoc) MarshalYAML() (interface{}, error) {
 	type alias TypedDoc
+	node := yaml.Node{}
+	err := node.Encode(alias(d))
+	if err != nil {
+		return nil, err
+	}
+	node.HeadComment = d.Comments.HeadComment
+	node.LineComment = d.Comments.LineComment
+	node.FootComment = d.Comments.FootComment
+	return node, nil
+}
+
+type Validity struct {
+	From        *HumanTime  `protobuf:"bytes,1,opt,name=from,proto3" json:"from,omitempty" yaml:"from,omitempty"`
+	Until       *HumanTime  `protobuf:"bytes,2,opt,name=until,proto3" json:"until,omitempty" yaml:"until,omitempty"`
+	FileContext FileContext `json:"-" yaml:"-"`
+	Comments    Comments    `json:"-" yaml:"-"`
+}
+
+func (x *Validity) UnmarshalYAML(node *yaml.Node) error {
+	// prevent recursive calls into UnmarshalYAML with a placeholder type
+	type tmp Validity
+	err := node.Decode((*tmp)(x))
+	if err != nil {
+		return err
+	}
+
+	x.FileContext.Column = node.Column
+	x.FileContext.Line = node.Line
+
+	headComment := node.HeadComment
+	if headComment == "" && len(node.Content) > 1 {
+		headComment = node.Content[0].HeadComment
+	}
+
+	lineComment := node.LineComment
+
+	footComment := node.FootComment
+	if footComment == "" && len(node.Content) > 1 {
+		last := len(node.Content) - 1
+		footComment = node.Content[last].FootComment
+	}
+
+	x.Comments.HeadComment = headComment
+	x.Comments.LineComment = lineComment
+	x.Comments.FootComment = footComment
+	return nil
+}
+
+func (d Validity) MarshalYAML() (interface{}, error) {
+	type alias Validity
 	node := yaml.Node{}
 	err := node.Encode(alias(d))
 	if err != nil {

commit 7095a958daebc34419c39b7353f72facb50b7796

🐛 Don't change score type
This turns out to be a breaking change. There's also no need to really
do this. Its not going to be counted against any policy and it looked
like this was only done for printing reasons

Broke in #1754

cli/reporter/print_compact.go
@@ -455,17 +455,18 @@ func (r *defaultReporter) printAssetQueries(resolved *policy.ResolvedPolicy, rep
 				sortedPassed = append(sortedPassed, id)
 			} else if score.Value >= uint32(r.ScoreThreshold) && r.ScoreThreshold != 0 {
 				sortedWarnings = append(sortedWarnings, id)
-			} else if score.Type == policy.ScoreType_Skip {
-				g := checkToPreview[query.Mrn]
-				var pg *previewGroup
-				if g != nil && g.Valid != nil && g.Valid.Until != nil {
-					pg = previewGroups[int(g.Valid.Until.Seconds)]
-				} else {
-					pg = previewGroups[math.MaxInt64]
-				}
-				pg.sortedFailures = append(pg.sortedFailures, id)
 			} else {
-				sortedFailed = append(sortedFailed, id)
+				if g, ok := checkToPreview[query.Mrn]; ok {
+					var pg *previewGroup
+					if g != nil && g.Valid != nil && g.Valid.Until != nil {
+						pg = previewGroups[int(g.Valid.Until.Seconds)]
+					} else {
+						pg = previewGroups[math.MaxInt64]
+					}
+					pg.sortedFailures = append(pg.sortedFailures, id)
+				} else {
+					sortedFailed = append(sortedFailed, id)
+				}
 			}
 		}
 

policy/executor/internal/nodes.go
@@ -560,12 +560,6 @@ func (nodeData *ReportingJobNodeData) score() (*policy.Score, error) {
 								s.Value = floor
 							}
 						}
-
-						// since we clone it from the child, which is just the raw score,
-						// we have to set the type on this layer
-						if c.impact.Scoring == explorer.ScoringSystem_IGNORE_SCORE {
-							s.Type = policy.ScoreType_Skip
-						}
 					}
 				}
 			}