f7bcddeb50e4f56f7e71e1c0d8df5271b8c23936
Christoph Hartmann <chris@lollyrock.com>
2025-11-01 13:47:18 +0100

    🧹 update cli test

--- test/cli/cli_test.go
+++ test/cli/cli_test.go
@@ -10,25 +10,41 @@ import (
 
 	cmdtest "github.com/google/go-cmdtest"
 	"github.com/spf13/cobra"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.mondoo.com/cnquery/v12/providers"
 	"go.mondoo.com/cnspec/v12/apps/cnspec/cmd"
 )
 
 var (
-	once      sync.Once
-	cnspecCmd *cobra.Command
+	once         sync.Once
+	cnspecCmd    *cobra.Command
+	providersDir string
 )
 
 func setup() {
 	var err error
+
+	providersDir = ".providers"
+
+	// we need to set the providers path to the temporary directory explicitly since the init function is called
+	// before the tests are run, therefore os.Setenv("PROVIDERS_PATH", providersDir) would be too late
+	providers.CustomProviderPath = providersDir
+	providers.DefaultPath = providersDir
+	providers.CachedProviders = nil
+
 	cnspecCmd, err = cmd.BuildRootCmd()
 	if err != nil {
 		panic(err)
 	}
+
 }
 
 func TestMain(m *testing.M) {
+	// remove the temporary providers directory after all tests have run
+	defer func() {
+		_ = os.RemoveAll(providersDir)
+	}()
+
 	ret := m.Run()
 	os.Exit(ret)
 }
@@ -48,19 +64,18 @@ func TestCompare(t *testing.T) {
 		}
 		return 0
 	})
-	tmpDirs := []string{}
 	ts.Setup = func(t string) error {
-		// Use a different providers dir for every test
-		providersDir, err := os.MkdirTemp(os.TempDir(), "providers-*")
+		// clean all files stored in the providers path
+		err := os.RemoveAll(providersDir)
 		if err != nil {
 			return err
 		}
-		tmpDirs = append(tmpDirs, providersDir)
-		return os.Setenv("PROVIDERS_PATH", providersDir)
+		err = os.MkdirAll(providersDir, 0o755)
+		if err != nil {
+			return err
+		}
+		// no other setup required
+		return nil
 	}
 	ts.Run(t, false) // set to true to update test files
-	for _, dir := range tmpDirs {
-		err := os.RemoveAll(dir)
-		assert.NoError(t, err, "failed to remove temporary providers dir %s", dir)
-	}
 }
