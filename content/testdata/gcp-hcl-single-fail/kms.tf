2eb3997393ae85b4e0b3f47ca1bf540657888f0d
Manuel Weber <manuel@mondoo.com>
2025-12-04 18:05:31 +0100

    fix/tf: fail single
    Signed-off-by: Manuel Weber <manuel@mondoo.com>

--- content/testdata/gcp-hcl-single-fail/kms.tf
+++ content/testdata/gcp-hcl-single-fail/kms.tf
@@ -1,28 +1,35 @@
+resource "random_id" "kms_suffix" {
+  byte_length = 2
+}
+
 resource "google_kms_key_ring" "keyring" {
-  name     = "keyring-example"
+  name     = "keyring-example-${random_id.kms_suffix.hex}"
   location = "global"
 }
 
 resource "google_kms_crypto_key" "key" {
-  name            = "crypto-key-example"
+  name            = "crypto-key-example-${random_id.kms_suffix.hex}"
   key_ring        = google_kms_key_ring.keyring.id
   rotation_period = "7776000s"
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
   }
 }
+
+# Get the current project's default service account for IAM binding
+data "google_project" "current" {}
 
 data "google_iam_policy" "admin" {
   binding {
     role = "roles/cloudkms.cryptoKeyEncrypter"
 
     members = [
-      "user:jane@example.com",
+      "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com",
     ]
   }
 }
 
 resource "google_kms_crypto_key_iam_policy" "crypto_key" {
   crypto_key_id = google_kms_crypto_key.key.id
-  policy_data = data.google_iam_policy.admin.policy_data
+  policy_data   = data.google_iam_policy.admin.policy_data
 }
