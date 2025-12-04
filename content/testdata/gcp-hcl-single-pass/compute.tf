ff161e9d5ebce6827e4cbe9032942ba0394970d1
Manuel Weber <manuel@mondoo.com>
2025-12-04 16:42:30 +0100

    tf/pass: add private ip connection
    Signed-off-by: Manuel Weber <manuel@mondoo.com>

--- content/testdata/gcp-hcl-single-pass/compute.tf
+++ content/testdata/gcp-hcl-single-pass/compute.tf
@@ -4,6 +4,21 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = true
 }
 
+# Private Service Access for Cloud SQL private IP connectivity
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
 resource "google_compute_instance" "default" {
   name         = "my-instance"
   machine_type = "n2-standard-2"
