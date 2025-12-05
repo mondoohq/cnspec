0ebdd9410ce6a324a4b8f109872fbdde37113f8a
Manuel Weber <manuel@mondoo.com>
2025-12-05 19:18:49 +0100

    mql/fix:  mondoo-gcp-security-cloud-sql-mysql-connections-require-ssl-tls-terraform-state ++
    Signed-off-by: Manuel Weber <manuel@mondoo.com>

--- content/testdata/gcp-hcl-single-fail/mysql.tf
+++ content/testdata/gcp-hcl-single-fail/mysql.tf
@@ -18,7 +18,8 @@ resource "google_sql_database_instance" "mysql_public_instance" {
       ipv4_enabled = true # Enable public IP
 
       // SSL connection encryption
-      #ssl_mode = "ALLOW_UNENCRYPTED_AND_ENCRYPTED"
+      // If not set, the default is to allow both encrypted and unencrypted connections.
+      ssl_mode = "ALLOW_UNENCRYPTED_AND_ENCRYPTED"
 
       # Configure authorized networks to restrict access
       # This limits public access to specific IP addresses
