ea6278efceb0f2fdd701366fd021e6ecefa77ee7
Manuel Weber <manuel@mondoo.com>
2025-12-05 15:51:50 +0100

    fix/tf: tiers more dependencies
    Signed-off-by: Manuel Weber <manuel@mondoo.com>

--- content/testdata/gcp-hcl-single-pass/compute.tf
+++ content/testdata/gcp-hcl-single-pass/compute.tf
@@ -18,6 +18,7 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.vpc_network.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  depends_on = [ google_compute_network.vpc_network.id ]
 }
 
 resource "google_compute_instance" "default" {
