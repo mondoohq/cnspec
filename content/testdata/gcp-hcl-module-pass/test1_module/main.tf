87209cb641685ca24279b6d87ad7b15bce308fe6
Manuel Weber <manuel@mondoo.com>
2025-12-06 00:51:35 +0100

    rm -r ../gcp-hcl-module-pass/
    Signed-off-by: Manuel Weber <manuel@mondoo.com>

--- content/testdata/gcp-hcl-module-pass/test1_module/main.tf
+++
@@ -1,6 +0,0 @@
-# Define the provider configuration
-provider "google" {
-  project = var.gcp_project_id
-  region  = var.region
-}
-
--- content/testdata/gcp-hcl-module-pass/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "gcp_project_id" {
-  description = "GCP Project ID"
-  type        = string
-  #default     = "your-project-name"
-}
