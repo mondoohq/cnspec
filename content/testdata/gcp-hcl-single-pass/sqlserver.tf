e8fa6295c9fee121c8c3b513e9d03999de2e0486
Manuel Weber <manuel@mondoo.com>
2025-12-05 15:50:01 +0100

    fix/tf: tiers db+ other fixes
    Signed-off-by: Manuel Weber <manuel@mondoo.com>

--- content/testdata/gcp-hcl-single-pass/sqlserver.tf
+++ content/testdata/gcp-hcl-single-pass/sqlserver.tf
@@ -3,11 +3,12 @@ resource "google_sql_database_instance" "sqlserver_public_instance" {
   name             = "sqlserver-pass-instance-${random_id.rnd.hex}"
   region           = var.region
   database_version = "SQLSERVER_2019_EXPRESS" # var.database_version
+  root_password    = var.user_password
 
   depends_on = [google_service_networking_connection.private_vpc_connection]
 
   settings {
-    tier = var.tier
+    tier = "db-custom-1-3840" # SQL Server requires custom tier
 
 
     # Configure IP connectivity - private IP only
