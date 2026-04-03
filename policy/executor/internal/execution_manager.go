f794d031aa3222d3c26b72db41b2bf3d0699755b
vjeffrey <vj@mondoo.com>
2026-04-03 07:21:14 -0600

    Remove slow query reporting to health platform (#2326)

--- policy/executor/internal/execution_manager.go
+++ policy/executor/internal/execution_manager.go
@@ -180,21 +180,6 @@ func (em *executionManager) executeCodeBundle(codeBundle *llx.CodeBundle, props 
 			Str("qrid", codeID).
 			Dur("duration", time.Since(startTime)).
 			Msg("finished query execution")
-		if time.Since(startTime) > 5*time.Minute {
-			// if the query duration was more than 5 minutes, send an alert to platform
-			health.ReportSlowQuery(
-				"cnspec", cnspec.Version, cnspec.Build,
-				health.SlowQueryInfo{
-					CodeID:   codeID,
-					Query:    codeBundle.Source,
-					Duration: time.Since(startTime),
-				},
-				health.WithTags(map[string]string{
-					"client.runtime.os":   goruntime.GOOS,
-					"client.runtime.arch": goruntime.GOARCH,
-				}),
-			)
-		}
 	}()
 	// TODO(jaym): sendResult may not be correct. We may need to fill in the
 	// checksum
